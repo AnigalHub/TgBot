ac46a7adcdcaf58e8837b3e42f44370f13b0502d
AnigalHub <anigal1997@gmail.com>
2021-12-13 21:43:22 +0300

    добавлен цикл на поиск ключевых слов

--- index.ts
+++ index.ts
@@ -9,16 +9,13 @@ bot
 
 bot.on('message',(msg) =>{
     const chatId = msg.chat.id //id
-
     let words = msg.text?.split(" ") //разбиение на элементы массива, "пробел"
     console.log(words) //массив
-
-    let a = words?.indexOf('через') //поиск в массиве (индекс)
-
     /*функция на вывод сообщения, которое напоминаем*/
     function sayHi(textMessage:string):void {
         bot.sendMessage(chatId,textMessage)
     }
+    /*функция с временем для повтора один раз*/
     function selectTime(word:string,phrase:string, timePhrase:number):void {
         if(word == "секунд" || word == "секунд" || word == "секунду"){
             setTimeout(sayHi, timePhrase*1000,phrase); console.log(timePhrase*1000)
@@ -45,22 +42,62 @@ bot.on('message',(msg) =>{
             bot.sendMessage(chatId,'Ошибка! Время не указано');
         }
     }
+
+    let keywordThrough = words?.indexOf('через') //поиск в массиве (индекс)
+    let keywordIn = words?.indexOf('в') //поиск в массиве (индекс)
+
+    let date = new Date();
+    console.log(date.getHours())
     /*проверка на undefined - слово, которое ищем и сам массив*/
-    if(a != undefined && words!=undefined){
-        console.log(a) //индекс слова в массиве
-        console.log(words[a+1]) //время
-        let time = parseInt(words[a+1])
-        console.log(time) //время
-        console.log(words[a+2])
-
-        let messageFuture = words.slice((a+3),words.length).join(' ')
-        console.log(messageFuture)
-
-        selectTime(words[a+2],messageFuture,time)
+    if(words!=undefined){
+        for (let word of words){
+            if (word == "через"){
+                let keywordThrough = words?.indexOf(word) //поиск в массиве (индекс)
+                console.log(keywordThrough) //индекс слова в массиве
+                console.log(words[keywordThrough+1]) //время
+                let time = parseInt(words[keywordThrough+1])
+                console.log(time) //время
+                console.log(words[keywordThrough+2])
+                let messageFuture = words.slice((keywordThrough+3),words.length).join(' ')
+                console.log(messageFuture)
+                selectTime(words[keywordThrough+2],messageFuture,time)
+            }
+            else if(word == "в"){
+                let keywordThrough = words?.indexOf(word) //поиск в массиве (индекс)
+                console.log(keywordThrough) //индекс слова в массиве
+                console.log(words[keywordThrough+1]) //время
+                let time = parseInt(words[keywordThrough+1])
+                console.log(time) //время
+                console.log(words[keywordThrough+2])
+                let messageFuture = words.slice((keywordThrough+3),words.length).join(' ')
+                console.log(messageFuture)
+                console.log(time-date.getHours())
+                selectTime(words[keywordThrough+2],messageFuture,(time-date.getHours()))
+            }
+        }
     }
 
-
-
+   /*if( keywordThrough != undefined && words!=undefined){
+        console.log(keywordThrough) //индекс слова в массиве
+        console.log(words[keywordThrough+1]) //время
+        let time = parseInt(words[keywordThrough+1])
+        console.log(time) //время
+        console.log(words[keywordThrough+2])
+        let messageFuture = words.slice((keywordThrough+3),words.length).join(' ')
+        console.log(messageFuture)
+        selectTime(words[keywordThrough+2],messageFuture,time)
+   }
+   else if(keywordIn != undefined && words!=undefined){
+        console.log(keywordIn) //индекс слова в массиве
+        console.log(words[keywordIn+1]) //время
+        let time = parseInt(words[keywordIn+1])
+        console.log(time) //время
+        console.log(words[keywordIn+2])
+        let messageFuture = words.slice((keywordIn+3),words.length).join(' ')
+        console.log(messageFuture)
+        selectTime(words[keywordIn+2],messageFuture,time)
+    }
+*/
     bot.sendMessage(chatId,'Привет');
 })
 
