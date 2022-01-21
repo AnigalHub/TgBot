8a33b0c96b41fa67a14e58e79e4d88c09dfdfcb1
AnigalHub <anigal1997@gmail.com>
2022-01-21 19:20:26 +0300

    добавлено две функции на перевод числа из строки в число (пятнадцать = 15)

--- index.ts
+++ index.ts
@@ -41,6 +41,139 @@ function ConvertTimeToMilliseconds(chatId:number, word:string,timePhrase:number)
     return ms
 }
 
+function ConvertSmallNumberFromStringToNumber(number:string) {
+    let numberTime:number
+    if(number == "одну" || number == "один"){
+        numberTime = 1
+    }
+    else if(number == "два" || number == "две" ){
+        numberTime = 2
+    }
+    else if(number == "три"){
+        numberTime = 3
+    }
+    else if(number == "четыре"){
+        numberTime = 4
+    }
+    else if(number == "пять"){
+        numberTime = 5
+    }
+    else if(number == "шесть"){
+        numberTime = 6
+    }
+    else if(number == "семь"){
+        numberTime = 7
+    }
+    else if(number == "восемь"){
+        numberTime = 8
+    }
+    else if(number == "девять"){
+        numberTime = 9
+    }
+    else if(number == "десять"){
+        numberTime = 10
+    }
+    else if(number == "одинадцать"){
+        numberTime = 11
+    }
+    else if(number == "двенадцать"){
+        numberTime = 12
+    }
+    else if(number == "тринадцать"){
+        numberTime = 13
+    }
+    else if(number == "четырнадцать"){
+        numberTime = 14
+    }
+    else if(number == "пятнадцать"){
+        numberTime = 15
+    }
+    else if(number == "шестнадцать"){
+        numberTime = 16
+    }
+    else if(number == "семнадцать"){
+        numberTime = 17
+    }
+    else if(number == "семнадцать"){
+        numberTime = 17
+    }
+    else if(number == "восемнадцать"){
+        numberTime = 18
+    }
+    else if(number == "девятнадцать"){
+        numberTime = 19
+    }
+    else if(number == "девятнадцать"){
+        numberTime = 19
+    }
+    else if(number == "двадцать"){
+        numberTime = 20
+    }
+    else if(number == "тридцать"){
+        numberTime = 30
+    }
+    else if(number == "сорок"){
+        numberTime = 40
+    }
+    else if(number == "пятьдесят"){
+        numberTime = 50
+    }
+    else if(number == "шестьдесят"){
+        numberTime = 60
+    }
+    else if(number == "семьдесят"){
+        numberTime = 70
+    }
+    else if(number == "восемьдесят"){
+        numberTime = 80
+    }
+    else if(number == "девяносто"){
+        numberTime = 90
+    }
+    else if(number == "сто"){
+        numberTime = 100
+    }
+    else{
+        numberTime = -1
+    }
+    return numberTime
+}
+function ConvertLargeNumberFromStringToNumber(number1:string,number2:string) {
+    let secondPartOfNumber:number = ConvertSmallNumberFromStringToNumber(number2)
+    let numberTime:number
+    if(number1 == "двадцать"){
+        numberTime = 20 + secondPartOfNumber
+    }
+    else if(number1 == "тридцать"){
+        numberTime = 30 + secondPartOfNumber
+    }
+    else if(number1 == "сорок"){
+        numberTime = 40 + secondPartOfNumber
+    }
+    else if(number1 == "пятьдесят"){
+        numberTime = 50 + secondPartOfNumber
+    }
+    else if(number1 == "шестьдесят"){
+        numberTime = 60 + secondPartOfNumber
+    }
+    else if(number1 == "семьдесят"){
+        numberTime = 70 + secondPartOfNumber
+    }
+    else if(number1 == "восемьдесят"){
+        numberTime = 80 + secondPartOfNumber
+    }
+    else if(number1 == "девяносто"){
+        numberTime = 90 + secondPartOfNumber
+    }
+    else if(number1 == "сто"){
+        numberTime = 100 + secondPartOfNumber
+    }
+    else{
+        numberTime = -1
+    }
+    return numberTime
+}
+
 /*функция разницы в днях между двумя датами*/
 function diffDates(day_one:Date, day_two:Date) {
     return (day_one.getTime() - day_two.getTime()) / (60 * 60 * 24 * 1000);
@@ -130,7 +263,7 @@ bot.on('message',(msg) =>{
                     messageFuture = words.slice((keywordInMessage+2),words.length).join(' ') //сообщение, которое напоминаем
                     millisecondsTime = ConvertTimeToMilliseconds(chatId,words[keywordInMessage+1],1)
                     setTimeout(() => bot.sendMessage(chatId, messageFuture),millisecondsTime); //функция со временем - когда напомнить + сообщение - что напоминаем
-
+                    
                     /**/
                     console.log(messageFuture)
                     CalculationOfFutureDateAndTime(millisecondsTime) /*дата в которую напоминаем сообщение*/
