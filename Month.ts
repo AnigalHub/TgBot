2e2e3bbe6ded81f094cb0d596f9e4abd83b0bacf
AnigalHub <anigal1997@gmail.com>
2022-02-22 22:31:31 +0300

    добавлено еще сокращение в месяцах

--- Month.ts
+++ Month.ts
@@ -18,6 +18,11 @@ export default  class Month {
             || this.month == 'сен' || this.month == 'окт' || this.month == 'ноя' || this.month == 'дек'){
             array=['янв','фев','мар','апр','май','июн','июл','авг','сен','окт','ноя','дек']
         }
+        else if(this.month == 'янв' || this.month == 'февр' || this.month == 'март' || this.month == 'апр'
+            || this.month == 'май' || this.month == 'июнь' || this.month == 'июль' || this.month == 'авг'
+            || this.month == 'сент' || this.month == 'октб' || this.month == 'нояб' || this.month == 'дек'){
+            array=['янв','февр','март','апр','май','июнь','июль','авг','сент','октб','нояб','дек']
+        }
         else if(this.month == 'январе' || this.month == 'феврале' || this.month == 'марте' || this.month == 'апреле'
             || this.month == 'мае' || this.month == 'июне' || this.month == 'июле' || this.month == 'августе'
             || this.month == 'сентябре' || this.month == 'октябре' || this.month == 'ноябре' || this.month == 'декабре'){
