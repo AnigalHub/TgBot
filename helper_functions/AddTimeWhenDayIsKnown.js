b4e9e8c2b9eb82aa58c4a63998c41b3149d56fbf
AnigalHub <anigal1997@gmail.com>
2022-04-06 20:08:21 +0300

    добавлены текущая дата и будущая в миллисекундах и переданы функции CountDifferenceInMillisecondsBetweenFutureAndCurrentDates

--- helper_functions/AddTimeWhenDayIsKnown.js
+++ helper_functions/AddTimeWhenDayIsKnown.js
@@ -8,47 +8,51 @@ var ConvertTime_1 = __importDefault(require("../ConvertTime"));
 var convertTime = new ConvertTime_1.default();
 var MessageToSend_1 = __importDefault(require("../MessageToSend"));
 //функция добавления времени, когда известен день
-function addTimeWhenDayIsKnown(bot, chatId, date, array, secondKeywordInMessage, millisecondsTime, messageFuture) {
-    if (array.includes('в') == true || array.includes('во') == true) {
-        if (array.includes('во') == true) {
-            array.splice(array.indexOf('во'), 1, 'в');
+function addTimeWhenDayIsKnown(date, words, secondKeywordInMessage, millisecondsTime, messageFuture) {
+    if (words.includes('в') == true || words.includes('во') == true) {
+        if (words.includes('во') == true) {
+            words.splice(words.indexOf('во'), 1, 'в');
         }
-        secondKeywordInMessage = array.indexOf('в');
-        if (millisecondsTime >= 86400000 && convertTime.ConvertTimeToMilliseconds(array[secondKeywordInMessage + 2], 1) < 86400000) {
+        secondKeywordInMessage = words.indexOf('в'); // номер ключевого слова в массиве
+        var arrayElementAfterSecondKeyword1 = words[(secondKeywordInMessage) + 1]; // элемент массива после ключевого слова (secondKeywordInMessage) - первый
+        var arrayElementAfterSecondKeyword2 = words[(secondKeywordInMessage) + 2]; // элемент массива после ключевого слова (secondKeywordInMessage) - второй
+        var arrayElementAfterSecondKeyword3 = words[(secondKeywordInMessage) + 3]; // элемент массива после ключевого слова (secondKeywordInMessage) - третий
+        var arrayElementAfterSecondKeyword4 = words[(secondKeywordInMessage) + 4]; // элемент массива после ключевого слова (secondKeywordInMessage) - четвертый
+        if (millisecondsTime >= 86400000 && convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword2, 1) < 86400000) {
             var futureDate = new Date(Date.parse(date.toString()) + millisecondsTime);
             futureDate.setHours(0, 0, 0, 0);
-            if (/^[0-9]*$/.test(array[secondKeywordInMessage + 1])) { //только цифры
-                var timeAfterSecondKeyword = parseInt(array[secondKeywordInMessage + 1]); //время с типом число
+            var dateMs = Date.parse(date.toString());
+            var futureDateMs = Date.parse(futureDate.toString());
+            if (/^[0-9]*$/.test(words[secondKeywordInMessage + 1])) { //только цифры
+                var timeAfterSecondKeyword = parseInt(arrayElementAfterSecondKeyword1); //время с типом число
                 if (timeAfterSecondKeyword == 0) {
                     timeAfterSecondKeyword = 24;
                 }
-                if (convertTime.ConvertTimeToMilliseconds(array[secondKeywordInMessage + 2], timeAfterSecondKeyword) == 0) { //проверка, что функция перевода времени в миллисекунды не возвращает 0 (ошибку)
+                if (convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword2, timeAfterSecondKeyword) == 0) { //проверка, что функция перевода времени в миллисекунды не возвращает 0 (ошибку)
                     throw new Error('Ошибка! Некорректно введено время. Пример: 10 сек | 15 секунд | 1 секунду | 3 секунды');
                 }
-                else if (timeAfterSecondKeyword > 24 && convertTime.ConvertTimeToMilliseconds(array[secondKeywordInMessage + 2], 1) >= 3600000) {
+                else if (timeAfterSecondKeyword > 24 && convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword2, 1) >= 3600000) {
                     throw new Error('Ошибка! Время не может быть больше 24');
                 }
                 else {
-                    messageFuture = array.slice((secondKeywordInMessage + 3), array.length).join(' '); //сообщение, которое напоминаем
-                    millisecondsTime = convertTime.CountDifferenceInMillisecondsBetweenFutureAndCurrentDates(date.getHours(), futureDate.getHours(), timeAfterSecondKeyword, array, secondKeywordInMessage + 2);
-                    //setTimeout(() => bot.sendMessage(chatId, messageFuture),millisecondsTime); //функция со временем - когда напомнить + сообщение - что напоминаем
+                    messageFuture = words.slice((secondKeywordInMessage + 3), words.length).join(' '); //сообщение, которое напоминаем
+                    millisecondsTime = convertTime.CountDifferenceInMillisecondsBetweenFutureAndCurrentDates(dateMs, futureDateMs, timeAfterSecondKeyword, words, secondKeywordInMessage + 2);
                     (0, DateAsString_1.DateAsString)(millisecondsTime, date);
                     return new MessageToSend_1.default(millisecondsTime, messageFuture);
                 }
             }
-            else if (/^[А-яЁё]*$/.test(array[secondKeywordInMessage + 1])) { // только буквы
-                if (convertTime.ConvertTimeToMilliseconds(array[secondKeywordInMessage + 1], 1) == 0 &&
-                    convertTime.ConvertTimeToMilliseconds(array[secondKeywordInMessage + 2], 1) == 0 &&
-                    convertTime.ConvertTimeToMilliseconds(array[secondKeywordInMessage + 3], 1) == 0 &&
-                    convertTime.ConvertTimeToMilliseconds(array[secondKeywordInMessage + 4], 1) == 0) {
+            else if (/^[А-яЁё]*$/.test(words[secondKeywordInMessage + 1])) { // только буквы
+                if (convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword1, 1) == 0 &&
+                    convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword2, 1) == 0 &&
+                    convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword3, 1) == 0 &&
+                    convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword4, 1) == 0) {
                     throw new Error('Ошибка! Не указана единица времени');
                 }
                 else {
-                    var timeAfterSecondKeyword = convertTime.ConvertLargeNumberFromStringToNumber(array[secondKeywordInMessage + 1], array[secondKeywordInMessage + 2]);
-                    var objTime = convertTime.CountTimeAsStringInMillisecondsAndAssembleMessage(timeAfterSecondKeyword, date.getTime(), futureDate.getTime(), array, secondKeywordInMessage + 1, secondKeywordInMessage + 2, secondKeywordInMessage + 3, secondKeywordInMessage + 4);
+                    var timeAfterSecondKeyword = convertTime.ConvertLargeNumberFromStringToNumber(arrayElementAfterSecondKeyword1, arrayElementAfterSecondKeyword2);
+                    var objTime = convertTime.CountTimeAsStringInMillisecondsAndAssembleMessage(timeAfterSecondKeyword, date.getTime(), futureDate.getTime(), words, secondKeywordInMessage + 1, secondKeywordInMessage + 2, secondKeywordInMessage + 3, secondKeywordInMessage + 4);
                     messageFuture = objTime.message;
                     millisecondsTime = objTime.millisecondsTime;
-                    // setTimeout(() => bot.sendMessage(chatId, messageFuture),millisecondsTime); //функция со временем - когда напомнить + сообщение - что напоминаем
                     (0, DateAsString_1.DateAsString)(millisecondsTime, date);
                     return new MessageToSend_1.default(millisecondsTime, messageFuture);
                 }
@@ -62,11 +66,9 @@ function addTimeWhenDayIsKnown(bot, chatId, date, array, secondKeywordInMessage,
         }
     }
     else {
-        //setTimeout(() => bot.sendMessage(chatId, messageFuture),millisecondsTime); //функция со временем - когда напомнить + сообщение - что напоминаем
         (0, DateAsString_1.DateAsString)(millisecondsTime, date);
         return new MessageToSend_1.default(millisecondsTime, messageFuture);
     }
-    //  return new MessageToSend(millisecondsTime, messageFuture)
 }
 exports.default = addTimeWhenDayIsKnown;
 //# sourceMappingURL=AddTimeWhenDayIsKnown.js.map
--- helper_functions/AddTimeWhenDayIsKnown.ts
+++ helper_functions/AddTimeWhenDayIsKnown.ts
@@ -5,43 +5,51 @@ const convertTime = new ConvertTime()
 import MessageToSend from "../MessageToSend";
 
 //функция добавления времени, когда известен день
-function addTimeWhenDayIsKnown(date:Date, array:Array<string>, secondKeywordInMessage:number,millisecondsTime:number,messageFuture:string) : MessageToSend {
-    if (array.includes('в') == true || array.includes('во') == true){
-        if(array.includes('во') == true){
-            array.splice(array.indexOf('во'),1,'в')
+function addTimeWhenDayIsKnown(date:Date, words:Array<string>, secondKeywordInMessage:number, millisecondsTime:number, messageFuture:string) : MessageToSend {
+    if (words.includes('в') == true || words.includes('во') == true){
+        if(words.includes('во') == true){
+            words.splice(words.indexOf('во'),1,'в')
         }
-        secondKeywordInMessage = array.indexOf('в')
-        if(millisecondsTime >= 86400000 && convertTime.ConvertTimeToMilliseconds(array[secondKeywordInMessage+2],1) < 86400000){
+        secondKeywordInMessage = words.indexOf('в') // номер ключевого слова в массиве
+        let arrayElementAfterSecondKeyword1 = words[(secondKeywordInMessage)+1] // элемент массива после ключевого слова (secondKeywordInMessage) - первый
+        let arrayElementAfterSecondKeyword2 = words[(secondKeywordInMessage)+2] // элемент массива после ключевого слова (secondKeywordInMessage) - второй
+        let arrayElementAfterSecondKeyword3 = words[(secondKeywordInMessage)+3] // элемент массива после ключевого слова (secondKeywordInMessage) - третий
+        let arrayElementAfterSecondKeyword4 = words[(secondKeywordInMessage)+4] // элемент массива после ключевого слова (secondKeywordInMessage) - четвертый
+
+        if(millisecondsTime >= 86400000 && convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword2,1) < 86400000){
             const futureDate = new Date (Date.parse(date.toString()) + millisecondsTime)
             futureDate.setHours(0,0,0,0)
-            if(/^[0-9]*$/.test(array[secondKeywordInMessage + 1])){ //только цифры
-                let timeAfterSecondKeyword = parseInt(array[secondKeywordInMessage+1]) //время с типом число
+            const dateMs = Date.parse(date.toString()) //дата сообщения в миллисекундах
+            const futureDateMs = Date.parse(futureDate.toString()) //будущая дата в миллисекундах
+
+            if(/^[0-9]*$/.test(words[secondKeywordInMessage + 1])){ //только цифры
+                let timeAfterSecondKeyword = parseInt(arrayElementAfterSecondKeyword1) //время с типом число
                 if(timeAfterSecondKeyword == 0){
                     timeAfterSecondKeyword = 24
                 }
-                if(convertTime.ConvertTimeToMilliseconds(array[secondKeywordInMessage+2],timeAfterSecondKeyword) == 0){ //проверка, что функция перевода времени в миллисекунды не возвращает 0 (ошибку)
+                if(convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword2,timeAfterSecondKeyword) == 0){ //проверка, что функция перевода времени в миллисекунды не возвращает 0 (ошибку)
                     throw new Error('Ошибка! Некорректно введено время. Пример: 10 сек | 15 секунд | 1 секунду | 3 секунды');
                 }
-                else if (timeAfterSecondKeyword > 24 && convertTime.ConvertTimeToMilliseconds(array[secondKeywordInMessage+2],1) >= 3600000){
+                else if (timeAfterSecondKeyword > 24 && convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword2,1) >= 3600000){
                     throw new Error('Ошибка! Время не может быть больше 24');
                 }
                 else {
-                    messageFuture = array.slice((secondKeywordInMessage+3),array.length).join(' ')//сообщение, которое напоминаем
-                    millisecondsTime = convertTime.CountDifferenceInMillisecondsBetweenFutureAndCurrentDates(date.getHours(),futureDate.getHours(),timeAfterSecondKeyword,array, secondKeywordInMessage+2)
+                    messageFuture = words.slice((secondKeywordInMessage+3),words.length).join(' ')//сообщение, которое напоминаем
+                    millisecondsTime = convertTime.CountDifferenceInMillisecondsBetweenFutureAndCurrentDates(dateMs,futureDateMs,timeAfterSecondKeyword,words, secondKeywordInMessage+2)
                     DateAsString(millisecondsTime,date)
                     return new MessageToSend(millisecondsTime, messageFuture)
                 }
             }
-            else if (/^[А-яЁё]*$/.test(array[secondKeywordInMessage + 1])) {// только буквы
-                if( convertTime.ConvertTimeToMilliseconds(array[secondKeywordInMessage+1],1) == 0 &&
-                    convertTime.ConvertTimeToMilliseconds(array[secondKeywordInMessage+2],1) == 0 &&
-                    convertTime.ConvertTimeToMilliseconds(array[secondKeywordInMessage+3],1) == 0 &&
-                    convertTime.ConvertTimeToMilliseconds(array[secondKeywordInMessage+4],1) == 0){
+            else if (/^[А-яЁё]*$/.test(words[secondKeywordInMessage + 1])) {// только буквы
+                if( convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword1,1) == 0 &&
+                    convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword2,1) == 0 &&
+                    convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword3,1) == 0 &&
+                    convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword4,1) == 0){
                     throw new Error('Ошибка! Не указана единица времени');
                 }
                 else {
-                    let timeAfterSecondKeyword :number =  convertTime.ConvertLargeNumberFromStringToNumber(array[secondKeywordInMessage+1], array[secondKeywordInMessage+2])
-                    let objTime = convertTime.CountTimeAsStringInMillisecondsAndAssembleMessage(timeAfterSecondKeyword,date.getTime(),futureDate.getTime(),array,secondKeywordInMessage+1,secondKeywordInMessage+2,secondKeywordInMessage+3,secondKeywordInMessage+4)
+                    let timeAfterSecondKeyword :number =  convertTime.ConvertLargeNumberFromStringToNumber(arrayElementAfterSecondKeyword1, arrayElementAfterSecondKeyword2)
+                    let objTime = convertTime.CountTimeAsStringInMillisecondsAndAssembleMessage(timeAfterSecondKeyword,date.getTime(),futureDate.getTime(),words,secondKeywordInMessage+1,secondKeywordInMessage+2,secondKeywordInMessage+3,secondKeywordInMessage+4)
                     messageFuture = objTime.message
                     millisecondsTime = objTime.millisecondsTime
 
