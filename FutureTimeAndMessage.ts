82ab35f73d01332d783698a0659a4bbecf1b7c0d
AnigalHub <anigal1997@gmail.com>
2022-04-08 22:43:12 +0300

    добавлен импорт addDayWhenTimeIsKnown

--- FutureTimeAndMessage.ts
+++ FutureTimeAndMessage.ts
@@ -4,6 +4,7 @@ import DayOfTheWeek from "./DayOfTheWeek";
 import DateAsString from "./helper_functions/DateAsString";
 import ConvertTime from "./ConvertTime";
 const convertTime = new ConvertTime()
+import addDayWhenTimeIsKnown from "./helper_functions/addDayWhenTimeIsKnown";
 
 
 export default class FutureTimeAndMessage{
@@ -356,25 +357,3 @@ function AddDateWhenItIsSpecifiedInFull(keywordInMessage:number,words:Array<stri
     }
 }
 
-//функция добавления времени, когда известен день
-function addDayWhenTimeIsKnown(date:Date,dayRemind:string,timeRemind:number,dateMs:number,words:Array<string>,keywordInMessage:number,messageFuture:string,millisecondsTime:number) : MessageToSend {
-    let futureDay = convertTime.ConvertWordIndicatorOfTimeToNumber(dayRemind)
-    if((timeRemind < date.getHours()) && dayRemind == 'сегодня'){
-        throw new Error('Ошибка! Время указано которое уже прошло - напомнить невозможно');
-    }
-    else if(convertTime.ConvertTimeToMilliseconds(dayRemind,1) >= 3600000){
-        throw new Error( 'Ошибка! Некорректно введено время и дата - неизвестно когда напоминать');
-    }
-    else if(futureDay  == -1){
-        throw new Error('Ошибка! Некорректно введена дата. Время указано, а дата нет.');
-    }
-    else {
-        let futureDate = new Date(date.getFullYear(), date.getMonth(), futureDay)
-        const futureDateMs = Date.parse(futureDate.toString()) //будущая дата в миллисекундах
-
-        millisecondsTime = convertTime.CountDifferenceInMillisecondsBetweenFutureAndCurrentDates(dateMs, futureDateMs, timeRemind, words, keywordInMessage + 2)
-        messageFuture = words.slice((keywordInMessage + 4), words.length).join(' ')//сообщение, которое напоминаем
-        DateAsString(millisecondsTime, date)
-        return new MessageToSend(millisecondsTime, messageFuture)
-    }
-}
