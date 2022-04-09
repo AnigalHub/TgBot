f5e38c15503ecb39d70c58f6e062b5aaa645e447
AnigalHub <anigal1997@gmail.com>
2022-04-09 19:25:58 +0300

    добавлены сообщения об ошибке и условия

--- helper_functions/AddDayWhenTimeAndDayOfTheWeekAreKnown.ts
+++ helper_functions/AddDayWhenTimeAndDayOfTheWeekAreKnown.ts
@@ -6,14 +6,21 @@ const convertTime = new ConvertTime()
 
 function addDayWhenTimeAndDayOfTheWeekAreKnown(numberKeywordInMessage:number,arrayElementWithDayOfTheWeek:string,arrayElementWhitTime:string,date:Date,words:Array<string>,timeMessage:number,time:number,messageFuture:string,millisecondsTime:number) : MessageToSend{
     let dayOfTheWeek = new DayOfTheWeek(arrayElementWithDayOfTheWeek)
-    let differenceInDays = dayOfTheWeek.DiffDaysOfTheWeek()
-    let futureDay = date.getDate() + differenceInDays
-    let futureDate = new Date(date.getFullYear(), date.getMonth(), futureDay)
+    if (dayOfTheWeek.SearchForTheDayNumberOfTheWeek() != -1){
+        if((convertTime.ConvertTimeToMilliseconds(arrayElementWhitTime,1) > 3600000) || (convertTime.ConvertTimeToMilliseconds(arrayElementWhitTime,1) == 1800000)){
+            throw new Error( 'Ошибка! Некорректно введено время. Вместо времени указана неккоректно дата или непонятное время');
+        }
+        else {
+            let differenceInDays = dayOfTheWeek.DiffDaysOfTheWeek()
+            let futureDay = date.getDate() + differenceInDays
+            let futureDate = new Date(date.getFullYear(), date.getMonth(), futureDay)
 
-    let futureMs = futureDate.getTime() + convertTime.ConvertTimeToMilliseconds(arrayElementWhitTime,time)
-    millisecondsTime = futureMs - timeMessage
-    messageFuture = words.slice((numberKeywordInMessage+5),words.length).join(' ')//сообщение, которое напоминаем
-    DateAsString(millisecondsTime,date)
-    return new MessageToSend(millisecondsTime, messageFuture)
+            let futureMs = futureDate.getTime() + convertTime.ConvertTimeToMilliseconds(arrayElementWhitTime,time)
+            millisecondsTime = futureMs - timeMessage
+            messageFuture = words.slice((numberKeywordInMessage+5),words.length).join(' ')//сообщение, которое напоминаем
+            DateAsString(millisecondsTime,date)
+            return new MessageToSend(millisecondsTime, messageFuture) }
+    }
+    throw new Error('Ошибка! Некорректно введен день (день недели). Пример: пн | пнд | понедельник ')
 }
 export default addDayWhenTimeAndDayOfTheWeekAreKnown
