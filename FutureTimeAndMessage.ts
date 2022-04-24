d094bb3180ce94073e8fb3672328d0873bcc4635
AnigalHub <anigal1997@gmail.com>
2022-04-24 22:34:52 +0300

    удалена функция DateAndTimeValidation

--- FutureTimeAndMessage.ts
+++ FutureTimeAndMessage.ts
@@ -284,24 +284,3 @@ export default class FutureTimeAndMessage{
         return addDateWhenItIsSpecifiedInFull(numberKeywordInMessage,numberKeywordInMessage + 3,this.words[numberKeywordInMessage],this.words,this.dateMessage,timeMessage, time, this.messageFuture,this.millisecondsTime)
     }
 }
-
-function DateAndTimeValidation(time:number,wordsElementAfterKeyword1:string,wordsElementAfterKeyword2:string):number {
-    if(time == 0){
-        time = 24
-    }
-    if(time > 24 && convertTime.ConvertTimeToMilliseconds(wordsElementAfterKeyword1,1) >= 3600000){
-        throw new Error('Ошибка! Время не может быть больше 24');
-    }
-    else if(!wordsElementAfterKeyword1){
-        throw new Error('Ошибка! Не указано тип времени (мин | сек | час)');
-    }
-    else if(convertTime.ConvertTimeToMilliseconds(wordsElementAfterKeyword1,time) == 0){ //проверка, что функция перевода времени в миллисекунды не возвращает 0 (ошибку)
-        throw new Error('Ошибка! Некорректно введено время. Пример: 10 сек | 15 минут | 9 часов');
-    }
-    else if(!wordsElementAfterKeyword2){
-      throw new Error('Ошибка! Не указана дата');
-   }
-    else {
-        return -1
-    }
-}
