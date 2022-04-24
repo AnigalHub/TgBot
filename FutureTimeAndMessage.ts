d8cad99160f28548ee9f3399b6d2fb1edc68e70f
AnigalHub <anigal1997@gmail.com>
2022-04-24 22:28:24 +0300

    добавлена проверка на существование чего-либо после цифры (времени)                              }

--- FutureTimeAndMessage.ts
+++ FutureTimeAndMessage.ts
@@ -318,6 +318,9 @@ function DateAndTimeValidation(time:number,wordsElementAfterKeyword1:string,word
     if(time > 24 && convertTime.ConvertTimeToMilliseconds(wordsElementAfterKeyword1,1) >= 3600000){
         throw new Error('Ошибка! Время не может быть больше 24');
     }
+    else if(!wordsElementAfterKeyword1){
+        throw new Error('Ошибка! Не указано тип времени (мин | сек | час)');
+    }
     else if(convertTime.ConvertTimeToMilliseconds(wordsElementAfterKeyword1,time) == 0){ //проверка, что функция перевода времени в миллисекунды не возвращает 0 (ошибку)
         throw new Error('Ошибка! Некорректно введено время. Пример: 10 сек | 15 минут | 9 часов');
     }
