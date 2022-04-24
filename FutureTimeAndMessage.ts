a5a33887243257c324948982a64c2c1b7b912e60
AnigalHub <anigal1997@gmail.com>
2022-04-24 23:09:04 +0300

    добавлена функция DateAndTimeValidation

--- FutureTimeAndMessage.ts
+++ FutureTimeAndMessage.ts
@@ -307,3 +307,20 @@ export default class FutureTimeAndMessage{
     }
 }
 
+function DateAndTimeValidation(time:number,wordsElementAfterKeyword1:string,wordsElementAfterKeyword2:string):number {
+    if(time == 0){
+        time = 24
+    }
+    if(time > 24 && convertTime.ConvertTimeToMilliseconds(wordsElementAfterKeyword1,1) >= 3600000){
+        throw new Error('Ошибка! Время не может быть больше 24');
+    }
+    else if(convertTime.ConvertTimeToMilliseconds(wordsElementAfterKeyword1,time) == 0){ //проверка, что функция перевода времени в миллисекунды не возвращает 0 (ошибку)
+        throw new Error('Ошибка! Некорректно введено время. Пример: 10 сек | 15 минут | 9 часов');
+    }
+    else if(!wordsElementAfterKeyword2){
+        throw new Error('Ошибка! Не указана дата');
+    }
+    else {
+        return -1
+    }
+}
