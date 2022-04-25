28e4f9dae91d0fa0061770cbf002b9274be33bd0
AnigalHub <anigal1997@gmail.com>
2022-04-25 19:47:51 +0300

    изменен нейминг переменных

--- helper_functions/DateAndTimeValidation.ts
+++ helper_functions/DateAndTimeValidation.ts
@@ -1,20 +1,20 @@
 import ConvertTime from '../ConvertTime'
 const convertTime = new ConvertTime()
 
-function dateAndTimeValidation(time:number,wordsElementAfterKeyword1:string,wordsElementAfterKeyword2:string):number {
+function dateAndTimeValidation(time:number,timeType:string,dateType:string):number {
     if(time == 0){
         time = 24
     }
-    if(time > 24 && convertTime.ConvertTimeToMilliseconds(wordsElementAfterKeyword1,1) >= 3600000){
+    if(time > 24 && convertTime.ConvertTimeToMilliseconds(timeType,1) >= 3600000){
         throw new Error('Ошибка! Время не может быть больше 24');
     }
-    else if(!wordsElementAfterKeyword1){
+    else if(!timeType){
         throw new Error('Ошибка! Не указано тип времени (мин | сек | час)');
     }
-    else if(convertTime.ConvertTimeToMilliseconds(wordsElementAfterKeyword1,time) == 0){ //проверка, что функция перевода времени в миллисекунды не возвращает 0 (ошибку)
+    else if(convertTime.ConvertTimeToMilliseconds(timeType,time) == 0){ //проверка, что функция перевода времени в миллисекунды не возвращает 0 (ошибку)
         throw new Error('Ошибка! Некорректно введено время. Пример: 10 сек | 15 минут | 9 часов');
     }
-    else if(!wordsElementAfterKeyword2){
+    else if(!dateType){
         throw new Error('Ошибка! Не указана дата');
     }
     else {
