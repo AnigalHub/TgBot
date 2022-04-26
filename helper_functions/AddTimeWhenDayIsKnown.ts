247010cf28d00dce2379473fae155180cc27afc2
AnigalHub <anigal1997@gmail.com>
2022-04-26 23:16:15 +0300

    добавлено условие - проверка на ввод сек/минут/часов больше чем существует в жизни

--- helper_functions/AddTimeWhenDayIsKnown.ts
+++ helper_functions/AddTimeWhenDayIsKnown.ts
@@ -33,11 +33,11 @@ function addTimeWhenDayIsKnown(date:Date, words:Array<string>, secondKeywordInMe
                 if(timeAfterSecondKeyword == 0){
                     timeAfterSecondKeyword = 24
                 }
-                if(convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword2,timeAfterSecondKeyword) == 0){ //проверка, что функция перевода времени в миллисекунды не возвращает 0 (ошибку)
-                    throw new Error('Ошибка! Некорректно введено время. Пример: 10 сек | 15 секунд | 1 секунду | 3 секунды');
+                if (timeAfterSecondKeyword > 24 && convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword2,1) == 3600000){
+                    throw new Error('Ошибка! Время не может быть больше 24 часов');
                 }
-                else if (timeAfterSecondKeyword > 24 && convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword2,1) >= 3600000){
-                    throw new Error('Ошибка! Время не может быть больше 24');
+                else if (timeAfterSecondKeyword > 59 && (convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword2,1) == 60000 || convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword2,1) == 1000)){
+                    throw new Error('Ошибка! Время не может быть больше 59 секунд/минут');
                 }
                 else {
                     messageFuture = words.slice((secondKeywordInMessage+3),words.length).join(' ')//сообщение, которое напоминаем
@@ -51,17 +51,32 @@ function addTimeWhenDayIsKnown(date:Date, words:Array<string>, secondKeywordInMe
                 if( convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword1,1) == 0 && convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword2,1) == 0 &&
                     convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword3,1) == 0 && convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword4,1) == 0){
 
-                    DateAsString(millisecondsTime,date)
-                    return new MessageToSend(millisecondsTime, messageFuture)
+                   DateAsString(millisecondsTime,date)
+                   return new MessageToSend(millisecondsTime, messageFuture)
                 }
                 else {
-                    let timeAfterSecondKeyword:number =  convertTime.ConvertLargeNumberFromStringToNumber(arrayElementAfterSecondKeyword1, arrayElementAfterSecondKeyword2)
-                    let objTime = convertTime.CountTimeAsStringInMillisecondsAndAssembleMessage(timeAfterSecondKeyword,dateMs,futureDateMs,words,secondKeywordInMessage+1,secondKeywordInMessage+2,secondKeywordInMessage+3,secondKeywordInMessage+4)
-                    messageFuture = objTime.message
-                    millisecondsTime = objTime.millisecondsTime
+                    let timeAfterSecondKeyword:number = convertTime.ConvertLargeNumberFromStringToNumber(arrayElementAfterSecondKeyword1, arrayElementAfterSecondKeyword2)
 
-                    DateAsString(millisecondsTime,date)
-                    return new MessageToSend(millisecondsTime, messageFuture)
+                    console.log(arrayElementAfterSecondKeyword1)
+                    if ( arrayElementAfterSecondKeyword1 != 'ноль' && timeAfterSecondKeyword == 0 &&
+                        convertTime.ConvertSmallNumberFromStringToNumber(arrayElementAfterSecondKeyword2) != 0 || convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword2,1) != 0){
+                      throw new Error('Ошибка! Некорректное время: опечатка или отсутствие');
+                    }
+                    else if(timeAfterSecondKeyword > 24 && convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword3,1) == 3600000 ){
+                        throw new Error('Ошибка! Время не может быть больше 24 часов');
+                    }
+                    else if(timeAfterSecondKeyword > 59 && (convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword3,1) == 60000 || convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword3,1) == 1000)){
+                        throw new Error('Ошибка! Время не может быть больше 59 секунд/минут');
+                    }
+                    else {
+                        let objTime = convertTime.CountTimeAsStringInMillisecondsAndAssembleMessage(timeAfterSecondKeyword,dateMs,futureDateMs,words,secondKeywordInMessage+1,secondKeywordInMessage+2,secondKeywordInMessage+3,secondKeywordInMessage+4)
+
+                        messageFuture = objTime.message
+                        millisecondsTime = objTime.millisecondsTime
+
+                        DateAsString(millisecondsTime,date)
+                        return new MessageToSend(millisecondsTime, messageFuture)
+                    }
                 }
             }
             else {
