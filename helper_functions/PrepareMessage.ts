565e1b9699455bae8cf8ac91e594bbf5adbf8ea0
AnigalHub <anigal1997@gmail.com>
2022-04-27 20:38:00 +0300

    добавлена функция удаления соседних дубликатных слов

--- helper_functions/PrepareMessage.ts
+++ helper_functions/PrepareMessage.ts
@@ -1,9 +1,11 @@
 import removeEmptyElementsFromArray from "./RemoveEmptyElementsFromArray";
+import removeDuplicateElementsFromArray from "./RemoveDuplicateElementsFromArray";
 
 //функция подготовки сообщения
 export default function prepareMessage(message:string){
     let text = message.toLocaleLowerCase()
     let words = text.split(" ") //разбиение на элементы массива, "пробел"
     words = removeEmptyElementsFromArray(words) // избавление в массиве от пустых элементов
+    words = removeDuplicateElementsFromArray (words) // избавление в массиве от пустых элементов
     return words;
 }
