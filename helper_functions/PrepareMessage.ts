9b81de945d170118884ab3b4e7ccf0e5fd66086d
AnigalHub <anigal1997@gmail.com>
2022-04-27 20:43:06 +0300

    изменен нейминг и импорт функции - удаления соседних дубликатных слов

--- helper_functions/PrepareMessage.ts
+++ helper_functions/PrepareMessage.ts
@@ -1,11 +1,11 @@
 import removeEmptyElementsFromArray from "./RemoveEmptyElementsFromArray";
-import removeDuplicateElementsFromArray from "./RemoveDuplicateElementsFromArray";
+import removeDuplicateAdjacentElementsFromArray from "./RemoveDuplicateAdjacentElementsFromArray";
 
 //функция подготовки сообщения
 export default function prepareMessage(message:string){
     let text = message.toLocaleLowerCase()
     let words = text.split(" ") //разбиение на элементы массива, "пробел"
     words = removeEmptyElementsFromArray(words) // избавление в массиве от пустых элементов
-    words = removeDuplicateElementsFromArray (words) // избавление в массиве от пустых элементов
+    words = removeDuplicateAdjacentElementsFromArray (words) // избавление в массиве от пустых элементов
     return words;
 }
