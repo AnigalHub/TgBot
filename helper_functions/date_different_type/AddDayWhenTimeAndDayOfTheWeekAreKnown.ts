93cf8e6ab7d369e52227e1c1c4f243e7a520acd2
AnigalHub <anigal1997@gmail.com>
2022-04-28 21:37:54 +0300

    убрана опечатка

--- helper_functions/date_different_type/AddDayWhenTimeAndDayOfTheWeekAreKnown.ts
+++ helper_functions/date_different_type/AddDayWhenTimeAndDayOfTheWeekAreKnown.ts
@@ -3,12 +3,13 @@ import DayOfTheWeek from "../../DayOfTheWeek";
 import ConvertTime from '../../ConvertTime'
 const convertTime = new ConvertTime()
 
-function addDayWhenTimeAndDayOfTheWeekAreKnown(numberKeywordInMessage:number,arrayElementWithDayOfTheWeek:string,arrayElementWhitTime:string,date:Date,words:Array<string>,timeMessage:number,time:number,messageFuture:string,millisecondsTime:number) : MessageToSend{
-    let startFutureMessage = words.indexOf(arrayElementWhitTime) +3
+function addDayWhenTimeAndDayOfTheWeekAreKnown(numberKeywordInMessage:number,arrayElementWithDayOfTheWeek:string,arrayElementWithTime:string,date:Date,words:Array<string>,timeMessage:number,time:number,messageFuture:string,millisecondsTime:number) : MessageToSend{
+    console.log(numberKeywordInMessage,arrayElementWithDayOfTheWeek,arrayElementWithTime,date,words,timeMessage,time,messageFuture,millisecondsTime)
+    let startFutureMessage = words.indexOf(arrayElementWithTime) +3
     let dayOfTheWeek = new DayOfTheWeek(arrayElementWithDayOfTheWeek)
 
     if (dayOfTheWeek.SearchForTheDayNumberOfTheWeek() != -1){
-        if((convertTime.ConvertTimeToMilliseconds(arrayElementWhitTime,1) > 3600000) || (convertTime.ConvertTimeToMilliseconds(arrayElementWhitTime,1) == 1800000)){
+        if((convertTime.ConvertTimeToMilliseconds(arrayElementWithTime,1) > 3600000) || (convertTime.ConvertTimeToMilliseconds(arrayElementWithTime,1) == 1800000)){
             throw new Error( 'Ошибка! Некорректно введено время. Вместо времени указана неккоректно дата или непонятное время');
         }
         else {
@@ -16,7 +17,7 @@ function addDayWhenTimeAndDayOfTheWeekAreKnown(numberKeywordInMessage:number,arr
             let futureDay = date.getDate() + differenceInDays
             let futureDate = new Date(date.getFullYear(), date.getMonth(), futureDay)
 
-            let futureMs = futureDate.getTime() + convertTime.ConvertTimeToMilliseconds(arrayElementWhitTime,time)
+            let futureMs = futureDate.getTime() + convertTime.ConvertTimeToMilliseconds(arrayElementWithTime,time)
             millisecondsTime = futureMs - timeMessage
             messageFuture = words.slice((startFutureMessage),words.length).join(' ')//сообщение, которое напоминаем
             return new MessageToSend(millisecondsTime, messageFuture) }
