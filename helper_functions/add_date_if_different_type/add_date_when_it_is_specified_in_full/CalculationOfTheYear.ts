72e84ec3b40871a9fd58f6d059805899fe06a86c
AnigalHub <anigal1997@gmail.com>
2022-04-29 19:18:50 +0300

    добавлена функция подсчета года - когда указана полная дата - для функции addDateWhenItIsSpecifiedInFull

---
+++ helper_functions/add_date_if_different_type/add_date_when_it_is_specified_in_full/CalculationOfTheYear.ts
@@ -0,0 +1,14 @@
+function CalculationOfTheYear(keyword:string, date:Date):number{
+   let yearMessage
+    if (keyword.length == 10) {
+        yearMessage = parseInt(keyword.substring(6, 12))
+    }
+    else if ((keyword.length == 8) && (String(date.getFullYear()).slice(2, 4) <= keyword.substring(6, 8))) {
+        yearMessage = parseInt(String(date.getFullYear()).slice(0, 2) + keyword.substring(6, 8))
+    }
+    else {
+        yearMessage = parseInt(String(parseInt(String(date.getFullYear()).slice(0, 2)) + 1) + keyword.substring(6, 8))
+    }
+    return yearMessage
+}
+export default CalculationOfTheYear
