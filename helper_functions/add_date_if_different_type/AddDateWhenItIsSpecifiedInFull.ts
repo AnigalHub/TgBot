c16aede9fbc1e75c6ea5a9bfe2e24a27f5fe311c
AnigalHub <anigal1997@gmail.com>
2022-04-30 19:35:49 +0300

    добавлен импорт ErrorHandlingOfIncorrectFullDateEntry и сама функция вместо кода

--- helper_functions/add_date_if_different_type/AddDateWhenItIsSpecifiedInFull.ts
+++ helper_functions/add_date_if_different_type/AddDateWhenItIsSpecifiedInFull.ts
@@ -2,6 +2,7 @@ import MessageToSend from "../../MessageToSend";
 import ConvertTime from "../../ConvertTime";
 const convertTime = new ConvertTime()
 import CalculationOfTheYear from "./add_date_when_it_is_specified_in_full/CalculationOfTheYear"
+import ErrorHandlingOfIncorrectFullDateEntry from "./add_date_when_it_is_specified_in_full/ErrorHandlingOfIncorrectFullDateEntry"
 
 function addDateWhenItIsSpecifiedInFull(numberKeywordInMessage:number,numberArrayElementResponsiveForTimeType:number, keyword:string, words:Array<string>,date:Date,timeMessageMs:number, time:number,messageFuture:string, millisecondsTime:number): MessageToSend {
     if  (keyword[2] != keyword[5] && (keyword.includes('.') == false || keyword.includes('/')== false
@@ -14,26 +15,8 @@ function addDateWhenItIsSpecifiedInFull(numberKeywordInMessage:number,numberArra
 
         let dayMessage = parseInt(keyword.substring(0, 2))
         let yearMessage = CalculationOfTheYear(keyword,date)
+        ErrorHandlingOfIncorrectFullDateEntry(dayMessage,monthMessage,yearMessage,date,words,numberArrayElementResponsiveForTimeType)
 
-        if (dayMessage > 31){
-            throw new Error( 'Ошибка! Некорректно введен день в дате. День не может быть больше 31. Максимальное число в месяце 30 или 31.');
-        }
-        else if (dayMessage == 0){
-            throw new Error( 'Ошибка! Некорректно введен день в дате. День не может быть меньше 01. Такого дня не существует.');
-        }
-        else if(monthMessage > 11){
-            throw new Error( 'Ошибка! Некорректно введен месяц в дате. Месяц не может быть больше 12. В году всего 12 месяцев');
-        }
-        else if(monthMessage < 0){
-            throw new Error( 'Ошибка! Некорректно введен месяц в дате. Месяц не может быть меньше 01. Год начинается с 01 месяца = январь.');
-        }
-        else if(yearMessage < date.getFullYear()){
-            throw new Error( 'Ошибка! Некорректно введен год в дате. Год меньше текущего. Напомнить в тот год, который уже прошел - невозможно!');
-        }
-        else if((convertTime.ConvertTimeToMilliseconds(words[numberArrayElementResponsiveForTimeType],1) > 3600000) || (convertTime.ConvertTimeToMilliseconds(words[numberArrayElementResponsiveForTimeType],1) == 1800000)){
-            throw new Error( 'Ошибка! Некорректно введено время. Вместо времени указана неккоректно дата или непонятное время');
-        }
-        else {
             let futureDate = new Date(yearMessage, monthMessage, dayMessage)
             const futureDateMs = Date.parse(futureDate.toString()) //будущая дата в миллисекундах
 
@@ -57,7 +40,7 @@ function addDateWhenItIsSpecifiedInFull(numberKeywordInMessage:number,numberArra
                 millisecondsTime = convertTime.CountDifferenceInMillisecondsBetweenFutureAndCurrentDates(timeMessageMs, futureDateMs, time, words, numberArrayElementResponsiveForTimeType)
             }
             return new MessageToSend(millisecondsTime, messageFuture)
-        }
+
     }
 }
 
