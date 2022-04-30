dab136568b3d89f7757aa846d507791e54d03f90
AnigalHub <anigal1997@gmail.com>
2022-04-30 20:36:07 +0300

    удалены избыточные параметры - которые не используются + неиспользуемый код

--- helper_functions/add_time_when_day_is_known/ErrorHandlingOfIncorrecrTimeEntry.ts
+++ helper_functions/add_time_when_day_is_known/ErrorHandlingOfIncorrecrTimeEntry.ts
@@ -1,18 +1,12 @@
 import ConvertTime from "./../../ConvertTime";
 const convertTime = new ConvertTime()
 
-function errorHandlingOfIncorrecrTimeEntry(timeAfterSecondKeyword:number,wordWithTypeOfTime:string, numberAsWord1:string, numberAsWord2:string) {
+function errorHandlingOfIncorrecrTimeEntry(timeAfterSecondKeyword:number,wordWithTypeOfTime:string) {
     if (timeAfterSecondKeyword > 24 && convertTime.ConvertTimeToMilliseconds(wordWithTypeOfTime,1) == 3600000){
         throw new Error('Ошибка! Время не может быть больше 24 часов');
     }
     if (timeAfterSecondKeyword > 59 && (convertTime.ConvertTimeToMilliseconds(wordWithTypeOfTime,1) == 60000 || convertTime.ConvertTimeToMilliseconds(wordWithTypeOfTime,1) == 1000)){
         throw new Error('Ошибка! Время не может быть больше 59 секунд/минут');
     }
-   // if ((numberAsWord1 != 'ноль' && numberAsWord1 != 'нуль' ) &&
-     //   ((convertTime.ConvertSmallNumberFromStringToNumber(numberAsWord1) == 0 && convertTime.ConvertTimeToMilliseconds(numberAsWord1,1) == 0 ) ||
-          //  (convertTime.ConvertSmallNumberFromStringToNumber(numberAsWord2) == 0 &&
-             //   convertTime.ConvertTimeToMilliseconds(wordWithTypeOfTime,1) !=0))){
-       // throw new Error('Ошибка! Некорректное время: опечатка или отсутствие');
-    //}
 }
 export default errorHandlingOfIncorrecrTimeEntry
