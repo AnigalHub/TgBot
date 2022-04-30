0ed38336f78ff82928545e805cba79a84d4ef67c
AnigalHub <anigal1997@gmail.com>
2022-04-30 20:03:37 +0300

    вызвана errorHandlingOfIncorrectDateOrTimeEntry, вместо кода

--- helper_functions/add_date_if_different_type/AddDayWhenTimeIsKnown.ts
+++ helper_functions/add_date_if_different_type/AddDayWhenTimeIsKnown.ts
@@ -7,25 +7,14 @@ import ErrorHandlingOfIncorrectDateOrTimeEntry from "./add_day_when_time_is_know
 function addDayWhenTimeIsKnown(date:Date, dayRemind:string, timeRemind:number, dateMs:number, words:Array<string>,
                                numberKeywordInMessage:number,numberArrayElementResponsiveForTimeType:number,
                                messageFuture:string, millisecondsTime:number) : MessageToSend {
+
     let futureDay = convertTime.ConvertWordIndicatorOfTimeToNumber(date,dayRemind)
+    ErrorHandlingOfIncorrectDateOrTimeEntry(date, dayRemind,timeRemind,futureDay)
+    let futureDate = new Date(date.getFullYear(), date.getMonth(), futureDay)
+    const futureDateMs = Date.parse(futureDate.toString())  //будущая дата в миллисекундах
 
-
-    if((timeRemind <= date.getHours()) && dayRemind == 'сегодня'){
-        throw new Error('Ошибка! Время указано которое уже прошло - напомнить невозможно');
-    }
-    else if(convertTime.ConvertTimeToMilliseconds(dayRemind,1) >= 3600000){
-        throw new Error( 'Ошибка! Некорректно введено время и дата - неизвестно когда напоминать');
-    }
-    else if(futureDay  == -1){
-        throw new Error('Ошибка! Некорректно введена дата. Возможно слитное написание');
-    }
-    else {
-        let futureDate = new Date(date.getFullYear(), date.getMonth(), futureDay)
-        const futureDateMs = Date.parse(futureDate.toString())  //будущая дата в миллисекундах
-
-        millisecondsTime = convertTime.CountDifferenceInMillisecondsBetweenFutureAndCurrentDates(dateMs, futureDateMs, timeRemind, words, numberArrayElementResponsiveForTimeType)
-        messageFuture = words.slice((numberArrayElementResponsiveForTimeType + 2), words.length).join(' ')//сообщение, которое напоминаем
-        return new MessageToSend(millisecondsTime, messageFuture)
-    }
+    millisecondsTime = convertTime.CountDifferenceInMillisecondsBetweenFutureAndCurrentDates(dateMs, futureDateMs, timeRemind, words, numberArrayElementResponsiveForTimeType)
+    messageFuture = words.slice((numberArrayElementResponsiveForTimeType + 2), words.length).join(' ')//сообщение, которое напоминаем
+    return new MessageToSend(millisecondsTime, messageFuture)
 }
 export default addDayWhenTimeIsKnown
