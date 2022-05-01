71ea8af03cce0a8f6e429935302b6b00742d9110
AnigalHub <anigal1997@gmail.com>
2022-05-01 20:52:55 +0300

    добавлена функция ErrorHandlingOfIncorrectTimeEntry

--- helper_functions/DateAndTimeValidation.ts
+++ helper_functions/DateAndTimeValidation.ts
@@ -1,27 +1,19 @@
 import ConvertTime from '../ConvertTime'
 const convertTime = new ConvertTime()
-
-function dateAndTimeValidation(time:number,timeType:string,dateType:string):number {
+import ErrorHandlingOfIncorrectTimeEntry from "./add_time_when_day_is_known/ErrorHandlingOfIncorrectTimeEntry"
+function dateAndTimeValidation(time:number,timeType:string,dateType:string){
     if(time == 0){
         time = 24
     }
-    if(time > 24 && convertTime.ConvertTimeToMilliseconds(timeType,1) >= 3600000){
-        throw new Error('Ошибка! Время не может быть больше 24');
-    }
-    else if(time > 59 && (convertTime.ConvertTimeToMilliseconds(timeType,1) == 60000 || convertTime.ConvertTimeToMilliseconds(timeType,1) == 1000)){
-        throw new Error('Ошибка! Время не может быть больше 59 секунд/минут');
-    }
-    else if(!timeType){
+    ErrorHandlingOfIncorrectTimeEntry(time,timeType)
+    if(!timeType){
         throw new Error('Ошибка! Не указано тип времени (мин | сек | час)');
     }
-    else if(convertTime.ConvertTimeToMilliseconds(timeType,time) == 0){ //проверка, что функция перевода времени в миллисекунды не возвращает 0 (ошибку)
+    if(convertTime.ConvertTimeToMilliseconds(timeType,time) == 0){ //проверка, что функция перевода времени в миллисекунды не возвращает 0 (ошибку)
         throw new Error('Ошибка! Некорректно введено время. Пример: 10 сек | 15 минут | 9 часов');
     }
-    else if(!dateType){
+    if(!dateType){
         throw new Error('Ошибка! Не указана дата');
-    }
-    else {
-        return -1
     }
 }
 
