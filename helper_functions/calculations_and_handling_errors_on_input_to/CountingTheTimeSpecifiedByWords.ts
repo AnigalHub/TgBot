fe2fb887745805fb8955c54e9079581eb6a97e84
AnigalHub <anigal1997@gmail.com>
2022-05-02 13:10:07 +0300

    функция countingTheTimeSpecifiedByWords - подсчет времени, указанного словами

---
+++ helper_functions/calculations_and_handling_errors_on_input_to/CountingTheTimeSpecifiedByWords.ts
@@ -0,0 +1,17 @@
+import ConvertTime from "./../../ConvertTime";
+const convertTime = new ConvertTime()
+
+function countingTheTimeSpecifiedByWords(element1:string,element2:string):number {
+    let time:number
+    if(convertTime.ConvertTimeToMilliseconds(element1,1) != 0){
+        time = 1
+    }
+    else if (convertTime.ConvertSmallNumberFromStringToNumber(element2) != 0){
+        time = convertTime.ConvertLargeNumberFromStringToNumber(element1, element2)
+    }
+    else {
+        time = convertTime.ConvertSmallNumberFromStringToNumber(element1)
+    }
+    return time
+}
+export default countingTheTimeSpecifiedByWords
