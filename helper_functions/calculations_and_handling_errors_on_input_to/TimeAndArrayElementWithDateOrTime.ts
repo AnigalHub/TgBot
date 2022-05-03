0044b0d381165d0e5813cbb41331701636f1bc69
AnigalHub <anigal1997@gmail.com>
2022-05-03 22:21:58 +0300

    класс TimeAndArrayElementWithDateOrTime с временем, элементом массива отвечающим за время и элементом массива отвечающим за дату

---
+++ helper_functions/calculations_and_handling_errors_on_input_to/TimeAndArrayElementWithDateOrTime.ts
@@ -0,0 +1,11 @@
+export  default class TimeAndArrayElementWithDateOrTime {
+    public time:number;
+    public arrayElementWithDate:string;
+    public numberArrayElementResponsiveForTimeType:number
+
+    constructor(time: number,arrayElementWithDate: string,numberArrayElementResponsiveForTimeType:number) {
+        this.time = time
+        this.arrayElementWithDate = arrayElementWithDate
+        this.numberArrayElementResponsiveForTimeType = numberArrayElementResponsiveForTimeType
+    }
+}
