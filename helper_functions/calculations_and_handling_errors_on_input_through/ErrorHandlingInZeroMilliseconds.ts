15bbfadbb1823f168c6e96cfca92a8c4c0903704
AnigalHub <anigal1997@gmail.com>
2022-05-04 15:25:15 +0300

    изменено сообщение об ошибке

--- helper_functions/calculations_and_handling_errors_on_input_through/ErrorHandlingInZeroMilliseconds.ts
+++ helper_functions/calculations_and_handling_errors_on_input_through/ErrorHandlingInZeroMilliseconds.ts
@@ -1,6 +1,9 @@
 function errorHandlingInZeroMilliseconds(milliseconds:number) {
     if(milliseconds == 0) {
-        throw new Error('Ошибка! Отсутствует или некорректно указана единица времени')
+        throw new Error('Ошибка! Неккоректное время или дата - неизвестно когда напоминать.\n' +
+            'Возможные ошибки:\n' +
+            '- неккоректно указано время\n' +
+            '- отсутствует или некорректно указана единица времени')
     }
 }
 export default errorHandlingInZeroMilliseconds
