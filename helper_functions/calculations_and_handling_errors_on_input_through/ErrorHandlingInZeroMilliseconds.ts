ef62c1d26efa185c01a8c11eef811b60d304379d
AnigalHub <anigal1997@gmail.com>
2022-05-04 15:39:39 +0300

    изменено сообщение об ошибке - изменена возможная ошибка

--- helper_functions/calculations_and_handling_errors_on_input_through/ErrorHandlingInZeroMilliseconds.ts
+++ helper_functions/calculations_and_handling_errors_on_input_through/ErrorHandlingInZeroMilliseconds.ts
@@ -2,8 +2,8 @@ function errorHandlingInZeroMilliseconds(milliseconds:number) {
     if(milliseconds == 0) {
         throw new Error('Ошибка! Неккоректное время или дата - неизвестно когда напоминать.\n' +
             'Возможные ошибки:\n' +
-            '- неккоректно указано время\n' +
-            '- отсутствует или некорректно указана единица времени')
+            '- неккоректно указано/отсутствие времени или даты \n' +
+            '- отсутствует или некорректно указана единица времени/даты')
     }
 }
 export default errorHandlingInZeroMilliseconds
