9fa9d1dea34e6ed93e2b8be2662372386f73c33e
AnigalHub <anigal1997@gmail.com>
2022-05-08 12:48:27 +0300

    изменено условие

--- helper_functions/add_date_if_different_type/ValidationOfTimeInput.ts
+++ helper_functions/add_date_if_different_type/ValidationOfTimeInput.ts
@@ -1,9 +1,12 @@
 import errorHandlingOfIncorrectTimeEntryUsingWords
     from "../add_time_when_day_is_known/ErrorHandlingOfIncorrectTimeEntryUsingWords";
+import ConvertTime from "../../ConvertTime";
+const convertTime = new ConvertTime()
 
-function validationOfTimeInput(words:Array<string>,arrayElementWithDate:string){
+function validationOfTimeInput(words:Array<string>,numberArrayElementResponsiveForTimeType:number){
     let wordIn:number = words.indexOf('в')
-    if (arrayElementWithDate == undefined){
+    console.log(words[numberArrayElementResponsiveForTimeType])
+    if (convertTime.ConvertTimeToMilliseconds(words[numberArrayElementResponsiveForTimeType],1) == 0){
         throw new Error('Ошибка! Неизвестный тип времени (сек|мин|час)');
     }
     if(!/^[0-9]*$/.test(words[wordIn+1])){
