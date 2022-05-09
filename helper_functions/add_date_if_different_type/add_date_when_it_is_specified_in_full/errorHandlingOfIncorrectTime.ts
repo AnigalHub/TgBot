020941a1e652bd818ad6c7abbda0eff7d5468690
AnigalHub <anigal1997@gmail.com>
2022-05-10 01:28:10 +0300

    добавлена checkingTheTypeOfTime

--- helper_functions/add_date_if_different_type/add_date_when_it_is_specified_in_full/errorHandlingOfIncorrectTime.ts
+++ helper_functions/add_date_if_different_type/add_date_when_it_is_specified_in_full/errorHandlingOfIncorrectTime.ts
@@ -1,11 +1,13 @@
 import ConvertTime from "../../../ConvertTime";
 const convertTime = new ConvertTime()
+import checkingTheTypeOfTime from "../../CheckingTheTypeOfTime";
+
 
 //функция - Обработка ошибок неверного ввода времени
 function errorHandlingOfIncorrectTime(words:Array<string>,numberArrayElementResponsiveForTimeType:number){
-    if (convertTime.ConvertTimeToMilliseconds(words[numberArrayElementResponsiveForTimeType],1) == 0){
-        throw new Error('<b>Ошибка! Неизвестный тип времени </b>\n'+'(сек | мин | час)');
-    }
+
+    checkingTheTypeOfTime(words,numberArrayElementResponsiveForTimeType)
+
     if((convertTime.ConvertTimeToMilliseconds(words[numberArrayElementResponsiveForTimeType],1) > 3600000) || (convertTime.ConvertTimeToMilliseconds(words[numberArrayElementResponsiveForTimeType],1) == 1800000)){
         throw new Error('<b>Ошибка! Некорректно введено время.</b>\n'+'Вместо времени указана неккоректно дата или непонятное время');
     }
