eb7cf144613aa7eca061f74647faa16762e7b2cd
AnigalHub <anigal1997@gmail.com>
2022-05-10 01:18:41 +0300

    удалено условие - на непонятный вариант даты

--- helper_functions/add_date_if_different_type/add_date_when_it_is_specified_in_full/CalculationOfTheYear.ts
+++ helper_functions/add_date_if_different_type/add_date_when_it_is_specified_in_full/CalculationOfTheYear.ts
@@ -1,13 +1,11 @@
+//функция - Подсчет года
 function calculationOfTheYear(keyword:string, date:Date):number{
-   let yearMessage:number
-    if (keyword.length == 10) {
+    let yearMessage:number = 0
+    if (keyword.length == 10) { // когда год: дд.мм.гггг
         yearMessage = parseInt(keyword.substring(6, 12))
     }
-    else if ((keyword.length == 8) && (String(date.getFullYear()).slice(2, 4) <= keyword.substring(6, 8))) {
+    if ((keyword.length == 8) && (String(date.getFullYear()).slice(2, 4) <= keyword.substring(6, 8))) { // когда год: дд.мм.гг
         yearMessage = parseInt(String(date.getFullYear()).slice(0, 2) + keyword.substring(6, 8))
-    }
-    else {
-        yearMessage = parseInt(String(parseInt(String(date.getFullYear()).slice(0, 2)) + 1) + keyword.substring(6, 8))
     }
     return yearMessage
 }
