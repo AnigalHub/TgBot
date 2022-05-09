cc05faec45918187ea082734f44c35dc96355943
AnigalHub <anigal1997@gmail.com>
2022-05-10 01:29:49 +0300

    добавлен комментарий - название к функции

--- helper_functions/CheckingTheTypeOfTime.ts
+++ helper_functions/CheckingTheTypeOfTime.ts
@@ -1,6 +1,7 @@
 import ConvertTime from "../../../ConvertTime";
 const convertTime = new ConvertTime()
 
+//функция - Проверка типа времени
 function checkingTheTypeOfTime(words:Array<string>,numberArrayElementResponsiveForTimeType:number) {
     if (convertTime.ConvertTimeToMilliseconds(words[numberArrayElementResponsiveForTimeType],1) == 0){
         throw new Error('<b>Ошибка! Неизвестный тип времени </b>\n'+'(сек | мин | час)');
