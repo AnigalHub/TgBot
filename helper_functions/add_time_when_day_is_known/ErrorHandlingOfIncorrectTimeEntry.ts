222ddf39313690e6334a71c83c738672714aeae3
AnigalHub <anigal1997@gmail.com>
2022-05-09 21:53:28 +0300

    добавлен текст про неправильный ввод

--- helper_functions/add_time_when_day_is_known/ErrorHandlingOfIncorrectTimeEntry.ts
+++ helper_functions/add_time_when_day_is_known/ErrorHandlingOfIncorrectTimeEntry.ts
@@ -3,10 +3,10 @@ const convertTime = new ConvertTime()
 
 function errorHandlingOfIncorrectTimeEntry(timeAfterSecondKeyword:number,wordWithTypeOfTime:string) {
     if (timeAfterSecondKeyword > 24 && convertTime.ConvertTimeToMilliseconds(wordWithTypeOfTime,1) == 3600000){
-        throw new Error('Ошибка! Время не может быть больше 24 часов');
+        throw new Error('<b>Ошибка! Некорректно введено время. </b>\n'+'Время не может быть больше 24 часов');
     }
     if (timeAfterSecondKeyword > 59 && (convertTime.ConvertTimeToMilliseconds(wordWithTypeOfTime,1) == 60000 || convertTime.ConvertTimeToMilliseconds(wordWithTypeOfTime,1) == 1000)){
-        throw new Error('Ошибка! Время не может быть больше 59 секунд/минут');
+        throw new Error('<b>Ошибка! Некорректно введено время. </b>\n'+'Время не может быть больше 59 секунд/минут');
     }
 }
 export default errorHandlingOfIncorrectTimeEntry
