f38ae1c111a259e32418a0b947f215ca725a391d
AnigalHub <anigal1997@gmail.com>
2022-05-10 13:34:13 +0300

    Добавлен комментарий - название функции. Удалены условия, которые не соответствуют названию функции

--- helper_functions/add_date_if_different_type/add_date_when_it_is_specified_in_full/ErrorHandlingOfIncorrectFullDateEntry.ts
+++ helper_functions/add_date_if_different_type/add_date_when_it_is_specified_in_full/ErrorHandlingOfIncorrectFullDateEntry.ts
@@ -1,6 +1,7 @@
 import ConvertTime from "../../../ConvertTime";
 const convertTime = new ConvertTime()
 
+//функция - Обработка ошибок неверного ввода полной даты
 function errorHandlingOfIncorrectFullDateEntry(keyword:string, dayMessage:number,monthMessage:number,yearMessage:number,date:Date,words:Array<string>,numberArrayElementResponsiveForTimeType:number){
     if  (keyword[2] != keyword[5] && (keyword.includes('.') == false || keyword.includes('/')== false
         || keyword.includes('-')== false) || (keyword.length > 10) || (keyword.length == 7) || (keyword.length == 9)) {
@@ -20,12 +21,6 @@ function errorHandlingOfIncorrectFullDateEntry(keyword:string, dayMessage:number
     }
     if(yearMessage < date.getFullYear()){
         throw new Error('<b>Ошибка! Некорректно введен год в дате. </b>\n'+'Год меньше текущего. Напомнить в тот год, который уже прошел - невозможно!');
-    }
-    if (convertTime.ConvertTimeToMilliseconds(words[numberArrayElementResponsiveForTimeType],1) == 0){
-        throw new Error('<b>Ошибка! Неизвестный тип времени </b>\n'+'(сек | мин | час)');
-    }
-    if((convertTime.ConvertTimeToMilliseconds(words[numberArrayElementResponsiveForTimeType],1) > 3600000) || (convertTime.ConvertTimeToMilliseconds(words[numberArrayElementResponsiveForTimeType],1) == 1800000)){
-        throw new Error('<b>Ошибка! Некорректно введено время.</b>\n'+'Вместо времени указана неккоректно дата или непонятное время');
     }
 }
 export default errorHandlingOfIncorrectFullDateEntry
