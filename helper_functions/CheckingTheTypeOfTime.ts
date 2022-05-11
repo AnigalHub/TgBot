29fa0ced99e4867ce5c20a1e52524725281e8998
AnigalHub <anigal1997@gmail.com>
2022-05-11 12:10:23 +0300

    изменен путь импорта

--- helper_functions/CheckingTheTypeOfTime.ts
+++ helper_functions/CheckingTheTypeOfTime.ts
@@ -1,8 +1,9 @@
-import ConvertTime from "../../../ConvertTime";
+import ConvertTime from "../ConvertTime";
 const convertTime = new ConvertTime()
 
 //функция - Проверка типа времени
 function checkingTheTypeOfTime(words:Array<string>,numberArrayElementResponsiveForTimeType:number) {
+    //проверка на правильный ввод типа времени - минута/час/секунда
     if (convertTime.ConvertTimeToMilliseconds(words[numberArrayElementResponsiveForTimeType],1) == 0){
         throw new Error('<b>Ошибка! Неизвестный тип времени </b>\n'+'(сек | мин | час)');
     }
