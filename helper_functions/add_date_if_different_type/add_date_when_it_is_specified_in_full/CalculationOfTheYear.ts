92b801737515cb40b59171dc70a6adca7c8249e1
AnigalHub <anigal1997@gmail.com>
2022-05-11 12:09:17 +0300

    добавлены комментарии

--- helper_functions/add_date_if_different_type/add_date_when_it_is_specified_in_full/CalculationOfTheYear.ts
+++ helper_functions/add_date_if_different_type/add_date_when_it_is_specified_in_full/CalculationOfTheYear.ts
@@ -1,10 +1,12 @@
 //функция - Подсчет года
 function calculationOfTheYear(keyword:string, date:Date):number{
     let yearMessage:number = 0
-    if (keyword.length == 10) { // когда год: дд.мм.гггг
+    //когда год: дд.мм.гггг
+    if (keyword.length == 10) {
         yearMessage = parseInt(keyword.substring(6, 12))
     }
-    if ((keyword.length == 8) && (String(date.getFullYear()).slice(2, 4) <= keyword.substring(6, 8))) { // когда год: дд.мм.гг
+    //когда год: дд.мм.гг
+    if ((keyword.length == 8) && (String(date.getFullYear()).slice(2, 4) <= keyword.substring(6, 8))) {
         yearMessage = parseInt(String(date.getFullYear()).slice(0, 2) + keyword.substring(6, 8))
     }
     return yearMessage
