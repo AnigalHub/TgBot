4ea76cc016a7886e83f74548b62c60b0b4cae2fb
AnigalHub <anigal1997@gmail.com>
2022-05-13 19:19:18 +0300

    функция сборки сообщения MessageToSend

---
+++ helper_functions/add_date_if_different_type/add_day_when_time_is_known/MessageAssembly.ts
@@ -0,0 +1,11 @@
+function MessageAssembly(words:Array<string>,arrayElementWithTime:string,arrayElementWithDayOfTheWeek:string):string {
+    let messageFuture:string
+    if (words.indexOf(arrayElementWithTime) < words.indexOf(arrayElementWithDayOfTheWeek)){
+        messageFuture = words.slice((words.indexOf(arrayElementWithDayOfTheWeek)+1),words.length).join(' ')//сообщение, которое напоминаем
+    }
+    else {
+        messageFuture = words.slice((words.indexOf(arrayElementWithTime) +1),words.length).join(' ')//сообщение, которое напоминаем
+    }
+    return messageFuture
+}
+export default MessageAssembly
