00db5a0a7d68c6def8446702201e1385e74e4e4c
AnigalHub <anigal1997@gmail.com>
2022-05-17 01:58:08 +0300

    добавлены комментарии на условия

--- helper_functions/add_date_if_different_type/add_date_when_it_is_specified_in_full/errorHandlingOfIncorrectTime.ts
+++ helper_functions/add_date_if_different_type/add_date_when_it_is_specified_in_full/errorHandlingOfIncorrectTime.ts
@@ -2,12 +2,12 @@ import ConvertTime from "../../../ConvertTime";
 const convertTime = new ConvertTime()
 import checkingTheTypeOfTime from "../../CheckingTheTypeOfTime";
 
-
 //функция - Обработка ошибок неверного ввода времени
 function errorHandlingOfIncorrectTime(words:Array<string>,numberArrayElementResponsiveForTimeType:number){
-
+    //проверка на правильный ввод типа времени - минута/час/секунда
     checkingTheTypeOfTime(words,numberArrayElementResponsiveForTimeType)
 
+    //проверка на то,что время не указано как дата - вместо времени: неделя/месяц/год
     if((convertTime.ConvertTimeToMilliseconds(words[numberArrayElementResponsiveForTimeType],1) > 3600000) || (convertTime.ConvertTimeToMilliseconds(words[numberArrayElementResponsiveForTimeType],1) == 1800000)){
         throw new Error('<b>Ошибка! Некорректно введено время.</b>\n'+'Вместо времени указана неккоректно дата или непонятное время');
     }
