5472c6173e18da819ebd28cf455b531ea70ccb1d
AnigalHub <anigal1997@gmail.com>
2022-05-17 21:24:36 +0300

    условиям добавлены комментарии

--- helper_functions/add_date_if_different_type/add_day_when_time_is_known/ErrorHandlingOfIncorrectDateOrTimeEntry.ts
+++ helper_functions/add_date_if_different_type/add_day_when_time_is_known/ErrorHandlingOfIncorrectDateOrTimeEntry.ts
@@ -3,12 +3,15 @@ const convertTime = new ConvertTime()
 
 //функция - Обработка ошибок неправильного ввода даты или времени
 function errorHandlingOfIncorrectDateOrTimeEntry(date:Date, dayRemind:string,timeRemind:number,futureDay:number) {
+    //проверка если указано "сегодня" и время меньше текущего
     if((timeRemind <= date.getHours()) && dayRemind == 'сегодня'){
         throw new Error('<b>Ошибка! Некорректно введено время. </b>\n'+ 'Время указано которое уже прошло - напомнить невозможно</b>');
     }
+    //проверка если указана единица измерения времени - не сек, мин, полчаса, час --- а день, нееделя, месяц, полгода, год
     if(convertTime.ConvertTimeToMilliseconds(dayRemind,1) >= 3600000){
         throw new Error('<b>Ошибка! Некорректно введено время и дата. </b>\n'+ 'Неизвестно когда напоминать</b>');
     }
+    //проверка если дата не сегодня, завтра, послезавтра, послепослезавтра - а что-то иное
     if(futureDay == -1){
         throw new Error('<b>Ошибка! Некорректно введена дата. </b>\n'+ 'Возможно слитное написание');
     }
