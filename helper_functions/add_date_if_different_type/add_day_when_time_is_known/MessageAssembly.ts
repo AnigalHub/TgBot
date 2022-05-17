f35f5cac68ff6a9c29fe00852527d1cd4f1b6dd5
AnigalHub <anigal1997@gmail.com>
2022-05-17 21:18:14 +0300

    добавлены комментарии

--- helper_functions/add_date_if_different_type/add_day_when_time_is_known/MessageAssembly.ts
+++ helper_functions/add_date_if_different_type/add_day_when_time_is_known/MessageAssembly.ts
@@ -1,9 +1,12 @@
 //функция - Сборка будущего сообщения
 function messageAssembly(words:Array<string>,arrayElementWithTime:string,arrayElementWithDayOfTheWeek:string):string {
     let messageFuture:string
+    //проверка на ввод дня недели и времени
+    //если время, а потом день недели
     if (words.indexOf(arrayElementWithTime) < words.indexOf(arrayElementWithDayOfTheWeek)){
         messageFuture = words.slice((words.indexOf(arrayElementWithDayOfTheWeek)+1),words.length).join(' ')//сообщение, которое напоминаем
     }
+    //если день недели, а потом время
     else {
         messageFuture = words.slice((words.indexOf(arrayElementWithTime) +1),words.length).join(' ')//сообщение, которое напоминаем
     }
