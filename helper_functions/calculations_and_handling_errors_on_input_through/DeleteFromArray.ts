0fb643cbdb9591b58517520f56cefe5f9d1216d2
AnigalHub <anigal1997@gmail.com>
2022-05-18 12:54:01 +0300

    добавленое описание функции

--- helper_functions/calculations_and_handling_errors_on_input_through/DeleteFromArray.ts
+++ helper_functions/calculations_and_handling_errors_on_input_through/DeleteFromArray.ts
@@ -1,4 +1,6 @@
+//функция - Удаление из массива
 function deleteFromArray(words:Array<string>,word:string) {
+    //проверка - содержится ли элемент в массиве
     if(words.indexOf(word) != -1){
         words.splice(words.indexOf(word),1)
     }
