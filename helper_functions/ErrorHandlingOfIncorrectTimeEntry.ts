81b69bef3701f80dd17da0f6f7552a5106a46f54
AnigalHub <anigal1997@gmail.com>
2022-05-18 13:03:58 +0300

    добавлены комментарии

--- helper_functions/ErrorHandlingOfIncorrectTimeEntry.ts
+++ helper_functions/ErrorHandlingOfIncorrectTimeEntry.ts
@@ -1,10 +1,13 @@
 import ConvertTime from "../ConvertTime";
 const convertTime = new ConvertTime()
 
+//функция - Обработка неправильного ввода времени
 function errorHandlingOfIncorrectTimeEntry(timeAfterSecondKeyword:number,wordWithTypeOfTime:string) {
+    //проверка когда время указано больше 24 часов
     if (timeAfterSecondKeyword > 24 && convertTime.ConvertTimeToMilliseconds(wordWithTypeOfTime,1) == 3600000){
         throw new Error('<b>Ошибка! Некорректно введено время. </b>\n'+'Время не может быть больше 24 часов');
     }
+    //проверка когда время указано больше 59 минут/секунд
     if (timeAfterSecondKeyword > 59 && (convertTime.ConvertTimeToMilliseconds(wordWithTypeOfTime,1) == 60000 || convertTime.ConvertTimeToMilliseconds(wordWithTypeOfTime,1) == 1000)){
         throw new Error('<b>Ошибка! Некорректно введено время. </b>\n'+'Время не может быть больше 59 секунд/минут');
     }
