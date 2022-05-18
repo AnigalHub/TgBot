7fff3db355449fd33653ef605675c20b445e8094
AnigalHub <anigal1997@gmail.com>
2022-05-18 21:38:40 +0300

    добавлены комментарии - название функции + условия

--- helper_functions/add_date_if_different_type/ValidationOfTimeInput.ts
+++ helper_functions/add_date_if_different_type/ValidationOfTimeInput.ts
@@ -1,14 +1,15 @@
-import errorHandlingOfIncorrectTimeEntryUsingWords
-    from "../ErrorHandlingOfIncorrectTimeEntryUsingWords";
+import errorHandlingOfIncorrectTimeEntryUsingWords from "../ErrorHandlingOfIncorrectTimeEntryUsingWords";
 import checkingTheTypeOfTime from "../CheckingTheTypeOfTime";
 
-
+//функция - Проверка ввода времени
 function validationOfTimeInput(words:Array<string>,numberArrayElementResponsiveForTimeType:number){
     let wordIn:number = words.indexOf('в')
-
+    //проверка типа времени
     checkingTheTypeOfTime(words,numberArrayElementResponsiveForTimeType)
 
+    //проверка - когда введено не число
     if(!/^[0-9]*$/.test(words[wordIn+1])){
+        //обработка неправильного ввода времени словами
         errorHandlingOfIncorrectTimeEntryUsingWords(words[wordIn+1],words[wordIn+2])
     }
 }
