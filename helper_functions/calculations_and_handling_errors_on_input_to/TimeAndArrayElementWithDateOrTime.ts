f75d380f84baf61316bccf0b13e82df30a5ad67a
AnigalHub <anigal1997@gmail.com>
2022-05-18 15:19:05 +0300

    добавлены комментарии на поля класса

--- helper_functions/calculations_and_handling_errors_on_input_to/TimeAndArrayElementWithDateOrTime.ts
+++ helper_functions/calculations_and_handling_errors_on_input_to/TimeAndArrayElementWithDateOrTime.ts
@@ -1,6 +1,10 @@
-export  default class TimeAndArrayElementWithDateOrTime {
+//Время, элемент массива с датой и номер элемента массива с типом времени
+export default class TimeAndArrayElementWithDateOrTime {
+    //время
     public time:number;
+    //элемент массива с датой (завтра/01.07.2023)
     public arrayElementWithDate:string;
+    //номер элемента массива с типом времени (сек/мин/час)
     public numberArrayElementResponsiveForTimeType:number
 
     constructor(time: number, arrayElementWithDate: string, numberArrayElementResponsiveForTimeType:number) {
