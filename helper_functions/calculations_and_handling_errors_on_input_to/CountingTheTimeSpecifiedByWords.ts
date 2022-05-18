617bdb542e7cdbce3bcd6e2b8536b40cdb4cb1fe
AnigalHub <anigal1997@gmail.com>
2022-05-18 15:19:47 +0300

    добавлены комментарии внутри функции

--- helper_functions/calculations_and_handling_errors_on_input_to/CountingTheTimeSpecifiedByWords.ts
+++ helper_functions/calculations_and_handling_errors_on_input_to/CountingTheTimeSpecifiedByWords.ts
@@ -3,14 +3,22 @@ const convertTime = new ConvertTime()
 
 //функция - Подсчет времени, указанного словами
 function countingTheTimeSpecifiedByWords(element1:string,element2:string):number {
+    //время
     let time:number
+
+    //проверка - первая часть времени - не является типом времени (пример: в двадцать минут)
     if(convertTime.ConvertTimeToMilliseconds(element1,1) != 0){
+        //время
         time = 1
     }
+    //проверка - вторая часть времени - не является типом времени (пример: в двадцать две минуты)
     else if (convertTime.ConvertSmallNumberFromStringToNumber(element2) != 0){
+        //время
         time = convertTime.ConvertLargeNumberFromStringToNumber(element1, element2)
     }
+    //проверка - первая часть и есть тип времени (пример: в минуту)
     else {
+        //время
         time = convertTime.ConvertSmallNumberFromStringToNumber(element1)
     }
     return time
