37c871ad12363406bf29b4ba01a7c92c0080b7a7
AnigalHub <anigal1997@gmail.com>
2022-05-19 00:43:14 +0300

    добавлены комментарии в методе CalculationsAndHandlingErrorsOnInputTo

--- FutureTimeAndMessage.ts
+++ FutureTimeAndMessage.ts
@@ -15,7 +15,7 @@ export default class FutureTimeAndMessage{
     private readonly words:Array<string>
     //дата отправки сообщения - объект Date
     private readonly dateMessage:Date
-    //миллисекунды - время когда напоминаем
+    //миллисекунды
     private millisecondsTime: number
     //сообщение, которое напоминаем
     private messageFuture: string = ''
@@ -33,20 +33,28 @@ export default class FutureTimeAndMessage{
         let wordsElementAfterKeyword1 = this.words[numberKeywordInMessage+1]
         // элемент массива после ключевого слова - второй
         let wordsElementAfterKeyword2 = this.words[numberKeywordInMessage+2]
+        //индексы ключевых слов - массив
         let keywordIndexes = Array.from(this.words.entries()).filter(i => i[1] == this.words[numberKeywordInMessage]).map(i => i[0])
+        //время
         let time:number
 
+        //проверка - если массив индексов ключевых слов больше 1 - т е ключевых слов больше 1 в сообщении
         if(keywordIndexes.length > 1){
             throw new Error('<b>Ошибка! Несколько раз указан указатель времени "ЧЕРЕЗ"</b>');
         }
+        //проверка - если "сегодня" было написано раньше слова "через"
         if (this.words.indexOf('сегодня') > this.words.indexOf('через')){
+            //удаление из массива
             deleteFromArray(this.words,'сегодня')
         }
+        //обработка ошибок неправильного времени и даты, введенной словами
         errorHandlingOfIncorrectTimeAndWordIndicatorOfDateEntry(this.words,numberKeywordInMessage)
 
         if(/^[0-9]*$/.test(wordsElementAfterKeyword1)){ // только цифры
             time = parseInt(wordsElementAfterKeyword1)
+            //сборка будущего сообщения
             this.messageFuture = this.words.slice((numberKeywordInMessage+3),this.words.length).join(' ')
+            //подсчет миллисекунд
             this.millisecondsTime = convertTime.ConvertTimeToMilliseconds(wordsElementAfterKeyword2,time)
             errorHandlingInZeroMilliseconds(this.millisecondsTime)
             return addTimeWhenDayIsKnown(this.dateMessage, this.words, this.millisecondsTime, this.messageFuture)
@@ -56,8 +64,11 @@ export default class FutureTimeAndMessage{
             if(convertTime.ConvertTimeToMilliseconds(wordsElementAfterKeyword1,1) != 0){
                 time = convertTime.ConvertTimeToMilliseconds(wordsElementAfterKeyword1,1)
             }
+            //объект, содержащий сообщение и миллисекунды
             let objTime = convertTime.CountTimeAsStringInMillisecondsAndAssembleMessage(time, timeMessage, timeMessage, this.words,numberKeywordInMessage+1,numberKeywordInMessage+2,numberKeywordInMessage+3,numberKeywordInMessage+4)
+            //сборка будущего сообщения
             this.messageFuture = objTime.message
+            //подсчет миллисекунд
             this.millisecondsTime = objTime.millisecondsTime
             errorHandlingInZeroMilliseconds(this.millisecondsTime)
             return addTimeWhenDayIsKnown(this.dateMessage, this.words,this.millisecondsTime,this.messageFuture)
@@ -67,17 +78,27 @@ export default class FutureTimeAndMessage{
         }
     }
     CalculationsAndHandlingErrorsOnInputTo(numberKeywordInMessage:number, timeMessage:number): MessageToSend{
-        let wordsElementAfterKeyword1 = this.words[numberKeywordInMessage+1] // элемент массива после ключевого слова - первый
+        //элемент массива после ключевого слова - первый
+        let wordsElementAfterKeyword1 = this.words[numberKeywordInMessage+1]
 
+        //время
         let time:number
+        //элемент массива с датой (завтра/01.07.2023)
         let arrayElementWithDate:string
+        //номер элемента массива с типом времени (сек/мин/час)
         let numberArrayElementResponsiveForTimeType:number
 
         if(/^[0-9]*$/.test(wordsElementAfterKeyword1) || /^[А-яЁё]*$/.test(wordsElementAfterKeyword1)) {
+            //объект, содержащий время и дату, введенные словами
             let obj = calculatingTimeAndDateInWords(this.dateMessage,this.words,numberKeywordInMessage)
+            //время
             time = obj.time
+            //элемент массива с датой (завтра/01.07.2023)
             arrayElementWithDate =obj.arrayElementWithDate
+            //номер элемента массива с типом времени (сек/мин/час)
             numberArrayElementResponsiveForTimeType = obj.numberArrayElementResponsiveForTimeType
+
+            //добавление даты разного типа (полная дата/день недели/словом указателем)
             return addDateOfDifferentType(this.dateMessage, arrayElementWithDate,numberArrayElementResponsiveForTimeType,time,timeMessage, this.words, numberKeywordInMessage,this.messageFuture, this.millisecondsTime)
         }
         else{
