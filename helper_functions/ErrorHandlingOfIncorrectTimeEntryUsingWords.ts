5ad0a6a91072fdb7118cfdc003737f28d96333f6
AnigalHub <anigal1997@gmail.com>
2022-05-18 14:37:55 +0300

    добавлено описание на переменные и условие

--- helper_functions/ErrorHandlingOfIncorrectTimeEntryUsingWords.ts
+++ helper_functions/ErrorHandlingOfIncorrectTimeEntryUsingWords.ts
@@ -3,9 +3,16 @@ const convertTime = new ConvertTime()
 
 //функция - Обработка неправильного ввода времени словами
 function errorHandlingOfIncorrectTimeEntryUsingWords(arrayElementAfterSecondKeyword1:string, arrayElementAfterSecondKeyword2:string) {
+
+    //время1 время2 тип_времени (пример: двадцать два часа)
+    //время1 - числом - когда arrayElementAfterSecondKeyword1 - число указано словами (одним словом)
     let number1: number = convertTime.ConvertSmallNumberFromStringToNumber(arrayElementAfterSecondKeyword1)
+    //время2 - числом - когда arrayElementAfterSecondKeyword1 - число указано словами (одним словом)
     let number2: number = convertTime.ConvertSmallNumberFromStringToNumber(arrayElementAfterSecondKeyword2)
 
+    //проверка - когда в первой или второй части числа ошибка (неправильный ввод числа словами):
+    //1. первая часть == 0 и это не тип времени (сек/мин/час)
+    //2. вторая часть == 0 и это не тип времени (сек/мин/час) и также первая часть тоже не тип времени (сек/мин/час)
     if((number1 == 0 && convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword1,1) == 0) ||
         (number2 == 0 && (convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword2,1) == 0)
         && (convertTime.ConvertTimeToMilliseconds(arrayElementAfterSecondKeyword1,1) == 0))){
