fd6bbe867278ac9efc915502e47cf56e737a235f
AnigalHub <anigal1997@gmail.com>
2022-05-18 12:49:14 +0300

    добавлены комментарии на все условия

--- helper_functions/add_date_if_different_type/AddDateWhenItIsSpecifiedInFull.ts
+++ helper_functions/add_date_if_different_type/AddDateWhenItIsSpecifiedInFull.ts
@@ -25,23 +25,36 @@ function addDateWhenItIsSpecifiedInFull(numberKeywordInMessage:number,numberArra
     //будущая дата в миллисекундах
     const futureDateMs = Date.parse(futureDate.toString())
 
-    if ( convertTime.ConvertTimeToMilliseconds(words[numberArrayElementResponsiveForTimeType],1) == 0){
+    //проверка - когда words[numberArrayElementResponsiveForTimeType] - является типом времени (сек/мин/час)
+    if (convertTime.ConvertTimeToMilliseconds(words[numberArrayElementResponsiveForTimeType],1) == 0){
+        //проверка - если время указано одним словом (один, пятнадцать)
         if (convertTime.ConvertSmallNumberFromStringToNumber(words[numberArrayElementResponsiveForTimeType]) != 0){
+            //сборка будущего сообщения
             messageFuture = words.slice((numberArrayElementResponsiveForTimeType+2),words.length).join(' ')
+            //подсчет миллисекунд
             millisecondsTime = convertTime.CountDifferenceInMillisecondsBetweenFutureAndCurrentDates(timeMessageMs, futureDateMs, time, words, numberArrayElementResponsiveForTimeType+1)
         }
+        // проверка - если время указано не одним словом
         else {
+            //сборка будущего сообщения
             messageFuture = words.slice((numberArrayElementResponsiveForTimeType),words.length).join(' ')//сообщение, которое напоминаем
+            //подсчет миллисекунд
             millisecondsTime = convertTime.CountDifferenceInMillisecondsBetweenFutureAndCurrentDates(timeMessageMs, futureDateMs, time, words, numberArrayElementResponsiveForTimeType-1)
         }
     }
+    //проверка - когда words[numberArrayElementResponsiveForTimeType] - не является типом времени (сек/мин/час)
     else {
+        //проверка - когда время указано раньше даты
         if(numberArrayElementResponsiveForTimeType < words.indexOf(keyword)){
+            //сборка будущего сообщения
             messageFuture = words.slice((numberArrayElementResponsiveForTimeType+2),words.length).join(' ')
         }
+        //проверка - когда дата указана раньше времени
         else {
+            //сборка будущего сообщения
             messageFuture = words.slice((numberArrayElementResponsiveForTimeType+1),words.length).join(' ')
         }
+        //подсчет миллисекунд
         millisecondsTime = convertTime.CountDifferenceInMillisecondsBetweenFutureAndCurrentDates(timeMessageMs, futureDateMs, time, words, numberArrayElementResponsiveForTimeType)
     }
     return new MessageToSend(millisecondsTime, messageFuture)
