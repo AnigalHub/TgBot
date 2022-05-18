8218bf33aa7319931db7d8dd8bbd1f0363992fac
AnigalHub <anigal1997@gmail.com>
2022-05-18 13:07:35 +0300

    добавлены комментарии - название функции и условия

--- helper_functions/DateAndTimeValidation.ts
+++ helper_functions/DateAndTimeValidation.ts
@@ -1,18 +1,26 @@
 import ConvertTime from '../ConvertTime'
 const convertTime = new ConvertTime()
 import ErrorHandlingOfIncorrectTimeEntry from "./ErrorHandlingOfIncorrectTimeEntry"
+
+//функция - Проверка даты и времени
 function dateAndTimeValidation(time:number,timeType:string,dateType:string){
+    //проверка - если время указано как 0
     if(time == 0){
         time = 24
+        return time
     }
+    //обработка неправильного ввода времени
     ErrorHandlingOfIncorrectTimeEntry(time,timeType)
 
+    //проверка - не указан тип времени (не введен)
     if(!timeType){
         throw new Error('<b>Ошибка! Не указан тип времени. </b>\n'+'(мин | сек | час)');
     }
+    //проверка - неккоректно введено время - тип времени
     if(convertTime.ConvertTimeToMilliseconds(timeType,time) == 0){
         throw new Error('<b>Ошибка! Некорректно введено время. </b>\n'+'Пример: 10 сек | 15 минут | 9 часов');
     }
+    //проверка - не указана дата (не введена)
     if(!dateType){
         throw new Error('<b>Ошибка! Не указана дата</b>');
     }
