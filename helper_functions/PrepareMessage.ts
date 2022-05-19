6988dae3ac5c0f664e5db29e7e65458210be4203
AnigalHub <anigal1997@gmail.com>
2022-05-19 15:26:57 +0300

    добавлен try/catch и функция errorHandlingRepeatingDifferentTypeOfTime

--- helper_functions/PrepareMessage.ts
+++ helper_functions/PrepareMessage.ts
@@ -1,17 +1,26 @@
 import removeEmptyElementsFromArray from "./RemoveEmptyElementsFromArray";
 import removeDuplicateAdjacentElementsFromArray from "./RemoveDuplicateAdjacentElementsFromArray";
+import DateAsString from "./DateAsString";
+import TelegramBot from "node-telegram-bot-api";
+import errorHandlingRepeatingDifferentTypeOfTime from "./ErrorHandlingRepeatingDifferentTypeOfTime";
 
 //функция - Подготовки сообщения
-export default function prepareMessage(message:string){
-    //изменение регистра букв сообщение - везде на маленькие
-    let text = message.toLocaleLowerCase()
-    //разбиение сообщение на слова в массив
-    let words = text.split(" ")
+export default async function prepareMessage(message:string,bot:TelegramBot,chatId:number){
 
-    //Удаление пустых элементов из массива
-    words = removeEmptyElementsFromArray(words)
+    try {
+        //изменение регистра букв сообщение - везде на маленькие
+        let text = message.toLocaleLowerCase()
+        //разбиение сообщение на слова в массив
+        let words = text.split(" ")
 
-    //Удаление повторяющихся соседних элементов из массива
-    words = removeDuplicateAdjacentElementsFromArray (words)
-    return words;
+        //Удаление пустых элементов из массива
+        words = removeEmptyElementsFromArray(words)
+
+        //Удаление повторяющихся соседних элементов из массива
+        words = removeDuplicateAdjacentElementsFromArray (words)
+        errorHandlingRepeatingDifferentTypeOfTime(words)
+        return words;
+    } catch (e:any) {
+        await bot.sendMessage(chatId,e.message,{parse_mode: 'HTML'})
+    }
 }
