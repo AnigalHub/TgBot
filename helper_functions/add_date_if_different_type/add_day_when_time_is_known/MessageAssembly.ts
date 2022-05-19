bfc783baee3f6c9b312c6c6750c2ce67f0b9f3d4
AnigalHub <anigal1997@gmail.com>
2022-05-19 19:54:46 +0300

    добавлен комментарий на messageFuture

--- helper_functions/add_date_if_different_type/add_day_when_time_is_known/MessageAssembly.ts
+++ helper_functions/add_date_if_different_type/add_day_when_time_is_known/MessageAssembly.ts
@@ -1,13 +1,17 @@
 //функция - Сборка будущего сообщения
 function messageAssembly(words:Array<string>,arrayElementWithTime:string,arrayElementWithDayOfTheWeek:string):string {
+    //будущее сообщение
     let messageFuture:string
+
     //проверка на ввод дня недели и времени
     //если время, а потом день недели
     if (words.indexOf(arrayElementWithTime) < words.indexOf(arrayElementWithDayOfTheWeek)){
+        //будущее сообщение
         messageFuture = words.slice((words.indexOf(arrayElementWithDayOfTheWeek)+1),words.length).join(' ')//сообщение, которое напоминаем
     }
     //если день недели, а потом время
     else {
+        //будущее сообщение
         messageFuture = words.slice((words.indexOf(arrayElementWithTime) +1),words.length).join(' ')//сообщение, которое напоминаем
     }
     return messageFuture
