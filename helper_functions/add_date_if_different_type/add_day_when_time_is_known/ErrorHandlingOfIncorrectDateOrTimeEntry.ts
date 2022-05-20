c249ca2fb47f49bd4debe42f324f61e63140e95a
AnigalHub <anigal1997@gmail.com>
2022-05-20 21:05:52 +0300

    исправлена ошибка - незакрытые теги в сообщении

--- helper_functions/add_date_if_different_type/add_day_when_time_is_known/ErrorHandlingOfIncorrectDateOrTimeEntry.ts
+++ helper_functions/add_date_if_different_type/add_day_when_time_is_known/ErrorHandlingOfIncorrectDateOrTimeEntry.ts
@@ -4,14 +4,18 @@ const convertTime = new ConvertTime()
 //функция - Обработка ошибок неправильного ввода даты или времени
 function errorHandlingOfIncorrectDateOrTimeEntry(date:Date, dayRemind:string,timeRemind:number,futureDay:number) {
     console.log('errorHandlingOfIncorrectDateOrTimeEntry')
+    console.log(dayRemind)
+    console.log(timeRemind)
+    console.log(date.getHours())
+    console.log(date.getMinutes())
 
     //проверка если указано "сегодня" и время меньше текущего
-    if((timeRemind <= date.getHours()) && dayRemind == 'сегодня'){
-        throw new Error('<b>Ошибка! Некорректно введено время. </b>\n'+ 'Время указано которое уже прошло - напомнить невозможно</b>');
+    if(timeRemind <= date.getHours() && dayRemind == 'сегодня'){
+        throw new Error('<b>Ошибка! Некорректно введено время. </b>\n'+ 'Время указано которое уже прошло - напомнить невозможно');
     }
     //проверка если указана единица измерения времени - не сек, мин, полчаса, час --- а день, нееделя, месяц, полгода, год
     if(convertTime.ConvertTimeToMilliseconds(dayRemind,1) >= 3600000){
-        throw new Error('<b>Ошибка! Некорректно введено время и дата. </b>\n'+ 'Неизвестно когда напоминать</b>');
+        throw new Error('<b>Ошибка! Некорректно введено время и дата. </b>\n'+ 'Неизвестно когда напоминать');
     }
     //проверка если дата не сегодня, завтра, послезавтра, послепослезавтра - а что-то иное
     if(futureDay == -1){
