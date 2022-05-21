b0baa192ad31b89b05033ab0d92552d516c4cb32
AnigalHub <anigal1997@gmail.com>
2022-05-21 17:44:53 +0300

    добавлена функция - Проверка сообщения на иностранные буквы

---
+++ helper_functions/CheckingMessageForForeignLetters.ts
@@ -0,0 +1,6 @@
+//функция - Проверка сообщения на иностранные буквы
+export default function checkingMessageForForeignLetters(message:string){
+    if ((!/^[?!,.а-яА-ЯёЁ0-9\s]+$/.test(message) && message[0] != '/')) {
+        throw Error('Ошибка! Не корректный ввод. Символы неизвестны - бот знает только русский язык!')
+    }
+}
