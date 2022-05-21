ef0651d5e31d7388f248c5adee1664220f2445d6
AnigalHub <anigal1997@gmail.com>
2022-05-21 17:45:10 +0300

    добавлен импорт checkingMessageForForeignLetters

--- helper_functions/PrepareMessage.ts
+++ helper_functions/PrepareMessage.ts
@@ -2,20 +2,18 @@ import removeEmptyElementsFromArray from "./RemoveEmptyElementsFromArray";
 import removeDuplicateAdjacentElementsFromArray from "./RemoveDuplicateAdjacentElementsFromArray";
 import TelegramBot from "node-telegram-bot-api";
 import errorHandlingRepeatingDifferentTypeOfTime from "./ErrorHandlingRepeatingDifferentTypeOfTime";
+import checkingMessageForForeignLetters from "./CheckingMessageForForeignLetters"
 
 //функция - Подготовки сообщения
 export default async function prepareMessage(message:string,bot:TelegramBot,chatId:number){
     try {
-        if (!(!/^[?!,.а-яА-ЯёЁ0-9\s]+$/.test(message) && message[0] != '/')) {
-            let text = message.toLocaleLowerCase()
-            let words = text.split(" ")
-            words = removeEmptyElementsFromArray(words)
-            words = removeDuplicateAdjacentElementsFromArray(words)
-            errorHandlingRepeatingDifferentTypeOfTime(words)
-            return words;
-        }
-        await bot.sendMessage(chatId,'Ошибка! Не корректный ввод. Символы неизвестны - бот знает только русский язык!',{parse_mode: 'HTML'})
-
+        checkingMessageForForeignLetters(message)
+        let text = message.toLocaleLowerCase()
+        let words = text.split(" ")
+        words = removeEmptyElementsFromArray(words)
+        words = removeDuplicateAdjacentElementsFromArray(words)
+        errorHandlingRepeatingDifferentTypeOfTime(words)
+        return words;
     } catch (e:any) {
         await bot.sendMessage(chatId,e.message,{parse_mode: 'HTML'})
     }
