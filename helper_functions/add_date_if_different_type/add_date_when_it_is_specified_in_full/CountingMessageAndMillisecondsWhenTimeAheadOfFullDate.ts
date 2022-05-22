b974489b128c2f32fa26c0d987c4678dc53ba21f
AnigalHub <anigal1997@gmail.com>
2022-05-22 19:55:40 +0300

    добавлена функция - подсчет сообщения и миллисекунд, когда время раньше полной даты

---
+++ helper_functions/add_date_if_different_type/add_date_when_it_is_specified_in_full/CountingMessageAndMillisecondsWhenTimeAheadOfFullDate.ts
@@ -0,0 +1,23 @@
+import ConvertTime from './../../../ConvertTime'
+const convertTime = new ConvertTime()
+import MessageFutureAndMilliseconds from "./MessageFutureAndMilliseconds";
+
+function countingMessageAndMillisecondsWhenTimeAheadOfFullDate(keyword:string,futureDateMs:number,numberArrayElementResponsiveForTimeType:number,
+                                                               words:Array<string>,timeMessageMs:number,
+                                                               time:number,messageFuture:string, millisecondsTime:number):MessageFutureAndMilliseconds {
+    //проверка - когда время указано раньше даты
+    if(numberArrayElementResponsiveForTimeType < words.indexOf(keyword)){
+        //сборка будущего сообщения
+        messageFuture = words.slice((numberArrayElementResponsiveForTimeType+2),words.length).join(' ')
+    }
+    //проверка - когда дата указана раньше времени
+    else {
+        //сборка будущего сообщения
+        messageFuture = words.slice((numberArrayElementResponsiveForTimeType+1),words.length).join(' ')
+    }
+    //подсчет миллисекунд
+    millisecondsTime = convertTime.CountDifferenceInMillisecondsBetweenFutureAndCurrentDates(timeMessageMs, futureDateMs, time, words, numberArrayElementResponsiveForTimeType)
+
+    return new MessageFutureAndMilliseconds(messageFuture, millisecondsTime)
+}
+export default countingMessageAndMillisecondsWhenTimeAheadOfFullDate
