c3c1be7f976e7d9b4d9a56a0114ae434df1ebdee
AnigalHub <anigal1997@gmail.com>
2022-05-23 01:34:59 +0300

    добавлен комментарии в теле функции

--- helper_functions/OptionsForCheckingTimeAndDate.ts
+++ helper_functions/OptionsForCheckingTimeAndDate.ts
@@ -6,19 +6,29 @@ function optionsForCheckingTimeAndDate(words:Array<string>, numberKeywordInMessa
 
     console.log('optionsForCheckingTimeAndDate')
 
+    //проверка - arrayElementWithDate - не является днем недели
     if(new DayOfTheWeek(arrayElementWithDate).SearchForTheDayNumberOfTheWeek() == -1){
+        //проверка - если элемент массива с датой равно undefined
         if (arrayElementWithDate == undefined){
+            //элемент массива с датой
             arrayElementWithDate = words[numberKeywordInMessage-1]
         }
         //Обработка ошибок неверного ввода времени
         errorHandlingOfIncorrectTime(timeRemind,words,words[numberArrayElementResponsiveForTimeType],arrayElementWithDate)
     }
+    //проверка - arrayElementWithDate - является днем недели
     else {
+        //элемент массива с типой времени (сек/мин/час)
         let arrayElementWithTimeType = words[numberArrayElementResponsiveForTimeType]
+
+        //проверка - если время состоит из двух слов: больше 20 и не делится без остатка на 10
         if(timeRemind > 20  && timeRemind%10 != 0 ){
+            //элемент массива с типой времени (сек/мин/час)
             arrayElementWithTimeType = words[numberArrayElementResponsiveForTimeType+1]
         }
+        //проверка - если время равно 1
         if (timeRemind == 1) {
+            //элемент массива с типой времени (сек/мин/час)
             arrayElementWithTimeType = words[numberArrayElementResponsiveForTimeType-1]
         }
         //Обработка ошибок неверного ввода времени
