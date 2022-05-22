b82b4a71701d58f9f640d0053a7ddb1eaf6b0c42
AnigalHub <anigal1997@gmail.com>
2022-05-23 01:24:24 +0300

    добавлены функция variationOfCheckingTimeAndDate

---
+++ helper_functions/VariationOfCheckingTimeAndDate.ts
@@ -0,0 +1,27 @@
+import DayOfTheWeek from "../DayOfTheWeek";
+import errorHandlingOfIncorrectTime from "./ErrorHandlingOfIncorrectTime";
+
+
+function variationOfCheckingTimeAndDate(words:Array<string>,numberKeywordInMessage:number,arrayElementWithDate:string,timeRemind:number,numberArrayElementResponsiveForTimeType:number){
+
+    console.log('variationOfCheckingTimeAndDate')
+
+    if(new DayOfTheWeek(arrayElementWithDate).SearchForTheDayNumberOfTheWeek() == -1){
+        if (arrayElementWithDate == undefined){
+            arrayElementWithDate = words[numberKeywordInMessage-1]
+        }
+        errorHandlingOfIncorrectTime(timeRemind,words,words[numberArrayElementResponsiveForTimeType],arrayElementWithDate)
+    }
+    else {
+        let arrayElementWithTimeType = words[numberArrayElementResponsiveForTimeType]
+        if(timeRemind > 20  && timeRemind%10 != 0 ){
+            arrayElementWithTimeType = words[numberArrayElementResponsiveForTimeType+1]
+        }
+        if (timeRemind == 1) {
+            arrayElementWithTimeType = words[numberArrayElementResponsiveForTimeType-1]
+        }
+        errorHandlingOfIncorrectTime(timeRemind,words,arrayElementWithTimeType,words[numberKeywordInMessage])
+    }
+}
+
+export default variationOfCheckingTimeAndDate
