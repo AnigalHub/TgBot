90cd54607d0523bcff96c169305f7841312f9d1b
AnigalHub <anigal1997@gmail.com>
2022-05-24 22:58:27 +0300

    добавлены комментарии в теле функции

--- helper_functions/SearchForTheNameOfTheMonth.ts
+++ helper_functions/SearchForTheNameOfTheMonth.ts
@@ -2,11 +2,15 @@ import Month from "../Month";
 
 //функция - Поиск названия месяца из сообщения
 function searchForTheNameOfTheMonth(words:Array<string>):string{
+    //название месяца
     let nameMonth = ''
+    //проходим по массиву - сообщению
     for(let i=0; i<words.length; i++){
         //месяц
         let month = new Month(words[i])
+        //проверка - если найден месяц в сообщении
         if(month.SearchForTheNumberOfTheMonth() != -1){
+            //название месяца
             nameMonth = words[i]
         }
     }
