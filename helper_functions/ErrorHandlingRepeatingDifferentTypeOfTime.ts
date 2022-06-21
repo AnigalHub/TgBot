1681e05facd12961cdb7e0927ca2e3c84efde099
AnigalHub <anigal1997@gmail.com>
2022-06-21 15:10:47 +0300

    изменен комментарий

--- helper_functions/ErrorHandlingRepeatingDifferentTypeOfTime.ts
+++ helper_functions/ErrorHandlingRepeatingDifferentTypeOfTime.ts
@@ -5,7 +5,7 @@ const convertTime = new ConvertTime()
 function errorHandlingRepeatingDifferentTypeOfTime(words:Array<string>) {
 //проход по массиву слов в сообщении
     for (let i= 0; i<words.length;i++){
-        //проверка на одинаковый ввод слова подряд
+        //проверка на повтор разного типа времени подряд
         if(convertTime.ConvertTimeToMilliseconds(words[i],1) != 0 && convertTime.ConvertTimeToMilliseconds(words[i+1],1) != 0){
             console.log('index.js')
             return await bot.sendMessage(chatId,'Ошибка! Подряд несколько раз указана единица времени')
