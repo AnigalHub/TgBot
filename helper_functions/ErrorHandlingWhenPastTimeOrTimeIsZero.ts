962668b956e285413f8f00da9188ea95df02cc8c
AnigalHub <anigal1997@gmail.com>
2022-07-01 12:18:17 +0300

    добавлено условие из удаленной функции - errorHandlingInZeroMilliseconds

---
+++ helper_functions/ErrorHandlingWhenPastTimeOrTimeIsZero.ts
@@ -0,0 +1,17 @@
+//функция - Обработка ошибок при прошлом времени или времени равным нулю
+function errorHandlingWhenPastTimeOrTimeIsZero(milliseconds:number) {
+    console.log('errorHandlingWhenPastTimeOrTimeIsZero')
+
+    //проверка, когда миллисекунд отрицательные
+    if(milliseconds < 0) {
+        throw new Error('<b>Ошибка! Некорректно введено время. </b>\n'+ 'Время указано которое уже прошло - напомнить невозможно');
+    }
+    //проверка, когда миллисекунд 0
+    if(milliseconds == 0) {
+        throw new Error('<b>Ошибка! Неккоректное время или дата - неизвестно когда напоминать.\n\n</b>' +
+            '<u>Возможные ошибки:\n</u>' +
+            '- неккоректно указано/отсутствие времени или даты \n' +
+            '- отсутствует или некорректно указана единица времени/даты')
+    }
+}
+export default errorHandlingWhenPastTimeOrTimeIsZero
--- helper_functions/errorHandlingWhenPastTimeOrTimeIsZero.ts
+++
@@ -1,9 +0,0 @@
-//функция - Обработка ошибок при прошлом времени или времени равным нулю
-function errorHandlingWhenPastTimeOrTimeIsZero(milliseconds:number) {
-    //проверка, когда миллисекунд отрицательные
-    if(milliseconds < 0) {
-        console.log('сheckingForPastTense')
-        throw new Error('<b>Ошибка! Некорректно введено время. </b>\n'+ 'Время указано которое уже прошло - напомнить невозможно');
-    }
-}
-export default errorHandlingWhenPastTimeOrTimeIsZero
