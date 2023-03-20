a8907ac861ada6d289c641bf5d91bb3e92a38623
AnigalHub <anigal1997@gmail.com>
2023-03-20 23:02:10 +0300

    создание таблицы tg_bot

--- sql_scripts/db.sql
+++ sql_scripts/db.sql
@@ -1,20 +1,12 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
--- DROP TABLE test
-CREATE TABLE test(
+-- DROP TABLE tg_bot
+CREATE TABLE tg_bot(
 	id uuid PRIMARY KEY DEFAULT (uuid_generate_v4()),
-	user_id bigInt,
+	telegram_user_id bigInt,
 	message text,
-	time bigInt,
+	message_date timestamp,
+	future_message_date timestamp,
 	status text
 )
 GRANT ALL ON test TO lang_user
-
-INSERT INTO test (text) VALUES ('world')
-
-SELECT * FROM test
-
-ALTER TABLE MessagesAndTime RENAME TO messages_and_time
-DROP TABLE messages_and_time
-
-SELECT * FROM MessagesAndTime
